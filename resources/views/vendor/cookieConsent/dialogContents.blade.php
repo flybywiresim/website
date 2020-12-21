17b63757b9aab4511a37e3d20273c89a6ec39ec7
Nathan Innes <nathaninnes001@gmail.com>
2020-12-21 23:44:15 +0000

    Added refuse to cookie consent

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,9 +1,16 @@
-<div class="js-cookie-consent cookie-consent">
-    <span class="cookie-consent__message align-self-center">
-        Your experience on this site will be improved by allowing cookies. See our <a target="_blank" href="https://drive.google.com/file/d/1KviQQWptR42079Vaxzijtf-2WxGTfB5E/view?usp=sharing">Privacy Policy</a> for more information.
-    </span>
+<div class="js-cookie-consent cookie-consent text-center">
+    <div class="container align-self-center">
+        <span class="cookie-consent__message">
+            Your experience on this site will be improved by allowing cookies. See our <a target="_blank" href="https://drive.google.com/file/d/1KviQQWptR42079Vaxzijtf-2WxGTfB5E/view?usp=sharing">Privacy Policy</a> for more information.
+        </span>
+    </div>
 
-    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-primary px-4 my-2 mx-2">
-        {{ trans('cookieConsent::texts.agree') }}
-    </button>
+    <div class="d-flex px-4 m-2">
+        <button class="js-cookie-consent-agree cookie-consent__agree btn btn-primary px-4">
+            {{ trans('cookieConsent::texts.agree') }}
+        </button>
+        <button class="btn btn-danger px-4 mx-2" onClick="history.go(-1);">
+            Refuse cookies
+        </button>
+    </div>
 </div>
