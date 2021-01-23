489002a6e58bfe95fa60ef0b239e1b1fdf85b137
benjozork <benjamin.dupont.984@gmail.com>
2021-01-23 01:44:56 -0500

    fix: add slight shadow to navbar when scrolled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,35 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      keyframes: {
-        wiggle: {
-          '0%, 100%': {transform: 'rotate(-1deg)'},
-          '50%': {transform: 'rotate(2.5deg)'},
-        }
-      },
-      animation: {
-        wiggle: 'wiggle 15s ease-in-out infinite'
-      },
-      backgroundColor: ['active'],
-      textColor: ['active'],
-      colors: {
-        blue: {
-          DEFAULT: '#6399AE',
-          light: '#00C2CB',
-          dark: '#1B2434',
-          darker: '#141E30'
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            keyframes: {
+                wiggle: {
+                    '0%, 100%': { transform: 'rotate(-1deg)' },
+                    '50%': { transform: 'rotate(2.5deg)' },
+                }
+            },
+            animation: {
+                wiggle: 'wiggle 15s ease-in-out infinite'
+            },
+            backgroundColor: ['active'],
+            textColor: ['active'],
+            colors: {
+                blue: {
+                    DEFAULT: '#6399AE',
+                    light: '#00C2CB',
+                    dark: '#1B2434',
+                    darker: '#141E30'
+                },
+            },
+            boxShadow: {
+                'lg-dark': '1px 1px 10px 1px rgba(0, 0, 0, 0.15)',
+                '2xl-light': '0 0 50px -20px rgba(255, 255, 255, 0.15)'
+            }
         },
-      },
-      boxShadow: {
-        '2xl-light': '0 0 50px -20px rgba(255, 255, 255, 0.15)'
-      }
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
