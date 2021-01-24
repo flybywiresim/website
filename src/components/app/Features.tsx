642e0db3ffa419b12b92ccd372c79ad1bffe83e3
benjozork <benjamin.dupont.984@gmail.com>
2021-01-23 22:52:59 -0500

    feat: add content to Features

--- src/components/app/Features.tsx
+++ src/components/app/Features.tsx
@@ -11,16 +11,16 @@ export function Features(): JSX.Element {
 
             <Container className="flex flex-wrap lg:flex-row items-stretch justify-center">
                 <Card className="bg-blue-dark shadow-2xl">
-                    <Title className="text-blue-light">Hello world</Title>
-                    <Body>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias cupiditate, debitis delectus distinctio facere hic impedit optio possimus praesentium sed!</Body>
+                    <Title className="text-blue-light">Systems Depth</Title>
+                    <Body>Unparalled systems depth on Microsoft Flight Simulator - ATSU/AOC, maintenance MCDU pages, fully featured ECAM pages</Body>
                 </Card>
                 <Card className="bg-blue-dark shadow-2xl">
-                    <Title className="text-blue-light">Hello world</Title>
-                    <Body>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias cupiditate, debitis delectus distinctio facere hic impedit optio possimus praesentium sed!</Body>
+                    <Title className="text-blue-light">Graphical and Audio Fidelity</Title>
+                    <Body>Tirelessly perfected sounds and visuals - based on thousands of IRL references for the utmost accuracy</Body>
                 </Card>
                 <Card className="bg-blue-dark shadow-2xl">
-                    <Title className="text-blue-light">Hello world</Title>
-                    <Body>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias cupiditate, debitis delectus distinctio facere hic impedit optio possimus praesentium sed!</Body>
+                    <Title className="text-blue-light">Approved by Real Pilots</Title>
+                    <Body>Constantly tested and tried by dozens of IRL A320 pilots and engineers, both in the development team and in the flight simulation community</Body>
                 </Card>
             </Container>
         </div>
