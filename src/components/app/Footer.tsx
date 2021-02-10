2cc5045cc0d1e6980d82048ecc8b78f6c4f2a92b
Arman Kirakosyan <twrblx@gmail.com>
2021-02-10 17:54:50 -0500

    feat: added underlines on the tos and pp (#54)

--- src/components/app/Footer.tsx
+++ src/components/app/Footer.tsx
@@ -31,10 +31,10 @@ export function Footer(): JSX.Element {
                     </div>
                     <div className="text-center">
                         <div className="grid grid-cols-2 w-full sm:w-3/4 mx-auto">
-                            <a href="https://github.com/flybywiresim/manuals/blob/master/pdf/Terms%20of%20Service.pdf" target="_blank" rel="noreferrer">
+                            <a className="hover:underline" href ="https://github.com/flybywiresim/manuals/blob/master/pdf/Terms%20of%20Service.pdf" target="_blank" rel="noreferrer">
                                 <p>Terms of Service</p>
                             </a>
-                            <a href="https://github.com/flybywiresim/manuals/blob/master/pdf/Privacy%20Policy.pdf" target="_blank" rel="noreferrer">
+                            <a className="hover:underline" href="https://github.com/flybywiresim/manuals/blob/master/pdf/Privacy%20Policy.pdf" target="_blank" rel="noreferrer">
                                 <p>Privacy Policy</p>
                             </a>
                         </div>
