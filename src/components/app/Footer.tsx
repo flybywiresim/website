ef6f6586ae541672d59a201c6b8c136eb45d9f89
Nathan Innes <nathaninnes001@gmail.com>
2021-02-10 20:02:00 +0000

    refactor: footer component rm duped code

--- src/components/app/Footer.tsx
+++ src/components/app/Footer.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
-import { faTwitter, faFacebook, faDiscord, faTwitch, faYoutube, faGithub } from '@fortawesome/free-brands-svg-icons';
+import React, { PropsWithChildren } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faDiscord, faFacebook, faGithub, faTwitch, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+type IconItemProp = {
+    icon: IconProp,
+    href?: string
+}
+
+export const FooterIconItem: React.FC<IconItemProp> = (props: PropsWithChildren<IconItemProp>) => {
+    return(
+        <a href={props.href} target="_blank" rel="noreferrer">
+            <FontAwesomeIcon className="mx-2" icon={props.icon} size="lg" />
+        </a>
+    );
+};
 
 export function Footer(): JSX.Element {
     return (
-        <div className="bg-blue-dark w-full m-auto px-10 py-8">
+        <div className="bg-blue-dark w-full px-10 py-8">
             <div className="flex flex-row justify-center">
                 <div className="flex flex-col">
                     <div className="flex flex-col-3 py-4 text-lg mx-auto">
-                        <a href="https://twitter.com/FlyByWireSim"><FontAwesomeIcon className="mx-2" icon={ faTwitter } size="lg" /></a>
-                        <a href="https://www.facebook.com/FlyByWireSimulations"><FontAwesomeIcon className="mx-2" icon={ faFacebook } size="lg" /></a>
-                        <a href="https://discord.gg/flybywire"><FontAwesomeIcon className="mx-2" icon={ faDiscord } size="lg" /></a>
-                        <a href="https://www.twitch.tv/flybywiresimulations"><FontAwesomeIcon className="mx-2" icon={ faTwitch } size="lg" /></a>
-                        <a href="https://www.youtube.com/c/FlyByWireSimulations"><FontAwesomeIcon className="mx-2" icon={ faYoutube } size="lg" /></a>
-                        <a href="https://github.com/flybywiresim"><FontAwesomeIcon className="mx-2" icon={ faGithub } size="lg" /></a>
+                        <FooterIconItem icon={faTwitter} href="https://twitter.com/FlyByWireSim" />
+                        <FooterIconItem icon={faFacebook} href="https://www.facebook.com/FlyByWireSimulations" />
+                        <FooterIconItem icon={faDiscord} href="https://discord.gg/flybywire" />
+                        <FooterIconItem icon={faTwitch} href="https://www.twitch.tv/flybywiresimulations" />
+                        <FooterIconItem icon={faYoutube} href="https://www.youtube.com/c/FlyByWireSimulations" />
+                        <FooterIconItem icon={faGithub} href="https://github.com/flybywiresim" />
                     </div>
                     <div className="text-center">
                         <div className="grid grid-cols-2 w-full sm:w-3/4 mx-auto">
@@ -26,7 +40,6 @@ export function Footer(): JSX.Element {
                         </div>
                         <p className="text-gray-400 py-2">&copy; FlyByWire Simulations 2021. All rights reserved.</p>
                     </div>
-
                 </div>
             </div>
         </div>
