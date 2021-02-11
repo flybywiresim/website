eea637e54555e2840e0fdbec060e8676f2d51f30
armankir <twrblx@gmail.com>
2021-02-11 08:36:51 -0500

    fix: remove rouge text

--- src/components/app/Download.tsx
+++ src/components/app/Download.tsx
@@ -53,7 +53,7 @@ export function Download(): JSX.Element {
                                     <div className="flex flex-row justify-between items-center mb-5 pt-5">
                                         <span className="text-xl text-gray-300">Development Build</span>
                                         <a href={getDownloadLink(urls.dev)}>
-                                            <Button className="w-40 float-right bg-blue-light-contrast AE hover:bg-blue-medium font-bold">Download</Button>
+                                            <Button className="w-40 float-right bg-blue-light-contrast hover:bg-blue-medium font-bold">Download</Button>
                                         </a>
                                     </div>
                                     <div className="flex flex-row justify-between items-center mb-8 pt-5">
