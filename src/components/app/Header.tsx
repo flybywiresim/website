2878751bf91886cd6207a36cd4e11d1bc31c3893
Nathan Innes <nathaninnes001@gmail.com>
2021-02-12 22:23:10 +0000

    refactor: code cleanup

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import { Button } from '../Button';
 import { Container } from '../Container';
 
-const c = {
-    emphLB: 'text-blue-light',
-    emphB: 'text-blue',
-};
-
 export function Header(): JSX.Element {
     return (
         <header id="header" className="AppHeader h-screen">
             <Container className="absolute max-w-6xl inset-x-2 inset-y-1/3 z-30">
                 <h1 data-aos="fade-right" data-aos-delay="500" className="text-7xl sm:text-8xl font-medium pb-3">
-                    <span className={c.emphLB}>A32</span><span className={c.emphB}>N</span>X
+                    <span className="text-blue-light">A32</span><span className="text-blue">N</span>X
                 </h1>
                 <p data-aos="fade-up" className="text-lg max-w-lg">A community built and maintained project aiming to provide high quality and detailed aircraft for Microsoft Flight
                     Simulator.</p>
