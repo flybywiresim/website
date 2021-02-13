3ad97df7ed16cf753d62b6880fdb31252ec915bd
Nathan Innes <nathaninnes001@gmail.com>
2021-02-13 02:46:42 +0000

    fix: logo size on nav

--- src/components/app/NavBar.tsx
+++ src/components/app/NavBar.tsx
@@ -20,7 +20,7 @@ export function NavBar(): JSX.Element {
             <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="relative flex items-center justify-between h-16">
                     <a href="#header">
-                        <img className="h-9" src={logo} alt=""/>
+                        <img className="h-11" src={logo} alt=""/>
                     </a>
                     <div className="absolute inset-y-0 right-3 flex items-center md:hidden">
                         <Hamburger handleClick={() => setOpen(prevState => !prevState)}/>
