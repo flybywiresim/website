597af03baf1d93d7b848feeebee21a0621b9c88e
Nathan Innes <nathaninnes001@gmail.com>
2021-02-14 15:48:19 +0000

    feat(navigation): navbar logo scroll to top

--- src/components/app/NavBar.tsx
+++ src/components/app/NavBar.tsx
@@ -3,6 +3,10 @@ import logo from '../../assets/svg/white/FBW-Logo-WHITE.svg';
 import { Hamburger, NavLinks } from './NavLinks';
 
 export function NavBar(): JSX.Element {
+    // Nav logo onClick scroll to top of the page.
+    const scrollTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
     const [scroll, setScroll] = useState(false);
     useEffect(() => {
         const listener = () => {
@@ -19,9 +23,7 @@ export function NavBar(): JSX.Element {
         <nav className={`fixed top-0 left-0 right-0 mx-auto py-2 z-50 ${scroll ? 'transition bg-blue-dark-contrast shadow-lg-dark' : 'transition transparent'}`}>
             <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="relative flex items-center justify-between h-16">
-                    <a href="#header">
-                        <img className="subpixel-antialiased h-11" src={logo} alt=""/>
-                    </a>
+                    <img onClick={scrollTop} className="subpixel-antialiased h-11" src={logo} alt=""/>
                     <div className="absolute inset-y-0 right-3 flex items-center md:hidden">
                         <Hamburger handleClick={() => setOpen(prevState => !prevState)}/>
                     </div>
