4001ac01ea57fb2c5c17a2de9bd1e1604863d3c3
Erick Torres <30361843+ErickSharp@users.noreply.github.com>
2021-02-14 16:53:25 -0600

    fix: change "Documentation" to "Learn More"

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from '../Button';
 
 type HeaderProps = {
-  scrollToDownload: () => void
+    scrollToDownload: () => void
 };
 
 export const Header: React.FC<HeaderProps> = (props) => (
@@ -12,22 +12,22 @@ export const Header: React.FC<HeaderProps> = (props) => (
                 <span className="text-blue-light">A32</span><span className="text-blue">N</span>X
             </h1>
             <p className="text-lg max-w-lg">A community built and maintained project aiming to provide high quality and
-                detailed aircraft for Microsoft Flight
+            detailed aircraft for Microsoft Flight
                 Simulator.</p>
             <div className="flex max-w-md py-5">
 
                 <Button onClick={props.scrollToDownload}
-                    className="w-40 bg-blue-light border-2 border-blue-light transition hover:bg-white hover:text-blue-light mr-3 font-medium">Download</Button>
+                    className="w-40 text-xl font-bold bg-blue-light border-2 border-blue-light transition hover:bg-white hover:text-blue-light mr-3">Download</Button>
 
                 <a href="https://docs.flybywiresim.com/" target="_blank" rel="noreferrer">
                     <Button
-                        className="w-40 border-2 border-blue-light text-blue-light transition hover:bg-white hover:text-blue-light font-medium">Documentation</Button>
+                        className="w-44 text-xl font-bold border-2 border-blue-light text-blue-light transition hover:bg-white hover:text-blue-light">Learn More</Button>
                 </a>
             </div>
         </div>
 
         <video className="absolute top-0 h-screen object-cover w-screen opacity-10"
             src="https://flybywiresim-packages.b-cdn.net/assets/website/A32NX.mp4" playsInline autoPlay
-            muted loop/>
+            muted loop />
     </header>
 );
