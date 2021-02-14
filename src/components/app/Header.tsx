f3ebb03c2f1c391bc8cf068bb4eb32ebbe67f5ab
armankir <twrblx@gmail.com>
2021-02-13 22:55:37 -0500

    feat: renamed nav links and learn more

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -14,8 +14,8 @@ export function Header(): JSX.Element {
                     <a href="#downloadRef">
                         <Button className="w-40 bg-blue-light border-2 border-blue-light transition hover:bg-white hover:text-blue-light mr-3 font-medium">Download</Button>
                     </a>
-                    <a href="https://docs.flybywiresim.com/" target="_blank" rel="noreferrer">
-                        <Button className="w-40 border-2 border-blue-light text-blue-light transition hover:bg-white hover:text-blue-light font-medium">Documentation</Button>
+                    <a>
+                        <Button className="w-40 border-2 border-blue-light text-blue-light transition hover:bg-white hover:text-blue-light font-medium">Learn More</Button>
                     </a>
                 </div>
             </div>
