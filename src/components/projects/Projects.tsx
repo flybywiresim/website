86f0961430bcbb349b39e3d0a88dd5bf8754f304
armankir <twrblx@gmail.com>
2021-02-15 15:21:22 -0500

    fix: change projects text

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -4,7 +4,7 @@ export const Projects = () => {
     return (
         <div className="mt-20 text-center">
             <p className="font-bold text-3xl">
-                Hi Sabes! Lol! Big Chungus!
+                Test
             </p>
             <img src="https://hg1.funnyjunk.com/pictures/Sexy+duck_709139_6636765.jpg" alt="duck" className="2xl:w-56 m-auto"/>
         </div>
