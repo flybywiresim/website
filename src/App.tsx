f6a856bb4d0ef511f86afad3e4b3c5e3314fd802
armankir <twrblx@gmail.com>
2021-02-15 01:21:11 -0500

    feat: setup react router and projects page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React, { useRef } from 'react';
 import './App.css';
 import { NavBar } from './components/app/NavBar';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
 import { Header } from './components/app/Header';
 import { Features } from './components/app/Features';
 import { Download } from './components/app/Download';
@@ -17,15 +23,27 @@ function App(): JSX.Element {
     };
 
     return (
-        <>
+        <Router>
             <NavBar />
-            <Header scrollToDownload={handleScrollTD} />
-            <Features ref={downloadRef} />
-            <Download />
-            <Community />
-            <PartnerSection />
-            <Footer/>
-        </>
+            <Switch>
+                <Route path="/projects">
+                    <div className="mt-20">
+                        <p>
+                            Test
+                        </p>
+                    </div>
+                </Route>
+                <Route path="/">
+                    <Header scrollToDownload={handleScrollTD} />
+                    <Features ref={downloadRef} />
+                    <Download />
+                    <Community />
+                    <PartnerSection />
+                    <Footer/>
+                </Route>
+
+            </Switch>
+        </Router>
     );
 }
 
