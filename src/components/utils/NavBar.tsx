636f0724188e4f0910b7d1c95580c2b28e183589
Erick Torres <30361843+ErickSharp@users.noreply.github.com>
2021-02-15 18:08:49 -0600

    @theomessin fucked up the merge

--- src/components/utils/NavBar.tsx
+++ src/components/utils/NavBar.tsx
@@ -24,9 +24,6 @@ export function NavBar(): JSX.Element {
         <nav className={`fixed top-0 left-0 right-0 mx-auto py-2 z-50 ${scroll ? 'transition bg-blue-dark-contrast shadow-lg-dark' : 'transition transparent'}`}>
             <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="relative flex items-center justify-between h-16">
-                    <a href="#header">
-                        <img className="subpixel-antialiased h-11" src={logo} alt="" />
-                    </a>
                     <img onClick={scrollTop} className="subpixel-antialiased h-11" src={logo} alt="" />
                     <div className="absolute inset-y-0 right-3 flex items-center md:hidden">
                         <Hamburger handleClick={() => setOpen(prevState => !prevState)} />
