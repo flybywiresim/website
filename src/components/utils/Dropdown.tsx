281075a0925e651aef5acdfc10a4df9a15fc939c
Nathan Innes <nathaninnes001@gmail.com>
2021-02-16 04:28:35 +0000

    feat: dropdown on hover

--- src/components/utils/Dropdown.tsx
+++ src/components/utils/Dropdown.tsx
@@ -11,13 +11,13 @@ export function DropdownItem(props: { name: string, link: string }): JSX.Element
 export const Dropdown: React.FC<{ titleName?: string, className?: string }> = (props) => {
     const [shown, setShown] = useState(false);
     return (
-        <div className={`relative inline-block text-left ${props.className}`}>
+        <div className={`relative inline-block text-left ${props.className}`} onMouseEnter={() => setShown(true)} onMouseLeave={() => setShown(false)}>
             <button draggable="false" type="button" onClick={() => setShown(old => !old)} className="inline-flex w-full px-4 py-2 text-xl text-white hover:text-blue-light transition-colors duration-200 focus:outline-none" id="options-menu" aria-haspopup="true" aria-expanded="true">
                 {props.titleName}
                 <FontAwesomeIcon className="h-5 w-5 self-center ml-1.5" icon={faChevronDown} size="xs" />
             </button>
 
-            {shown && <div className="block my-3 md:absolute mt-2 mx-4 md:min-w-max rounded-lg bg-blue-dark-contrast md:bg-white md:ring-1 md:ring-gray-500 ring-opacity-5 ">
+            {shown && <div className="block md:absolute mx-4 md:min-w-max rounded-lg bg-blue-dark-contrast md:bg-white md:ring-1 md:ring-gray-500 ring-opacity-5 ">
                 <div role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                     {props.children}
                 </div>
