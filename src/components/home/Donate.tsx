e1df7df26b9d9ef0a65343ac83aa231c104d5bc0
benjozork <benjamin.dupont.984@gmail.com>
2021-02-16 23:05:16 -0500

    feat: add donate section

---
+++ src/components/home/Donate.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import BackgroundImage from '../../assets/img/donate.jpg';
+import { Button } from '../Button';
+
+export const Donate: React.FC = () => {
+    return (
+        <section className="h-120 flex flex-row items-center">
+            <div className="absolute w-full z-10 flex flex-row justify-center">
+                <div className="w-3/4 xl:w-2/3 flex flex-col text-center xl:text-left">
+                    <div>
+                        <h1 className="text-5xl text-teal font-extrabold">Donate</h1>
+
+                        <p className="xl:w-2/3 text-xl mt-6">
+                            Contributions allow FlyByWire Simulations to provide better services for our users, such as fast and reliable download networks and integrated online features to enrich your flight simulation experience.
+                            We use Open Collective to transparently manage our donations and expenses, which means you can see how we utilize donated funds at any time.
+                        </p>
+                    </div>
+
+                    <div className="mt-6 xl:divide-x divide-gray-400 xl:space-x-7 flex flex-col-reverse xl:flex-row items-center">
+                        <a href="https://opencollective.com/flybywire/" target="_blank" rel="noreferrer">
+                            <Button className="w-40 text-xl font-bold bg-teal border-2 border-blue-light transition hover:bg-white hover:text-blue-light">Donate</Button>
+                        </a>
+                        <h2 className="max-w-prose text-teal-50 h-14 mb-7 xl:mb-0 xl:pl-7 flex flex-row items-center">
+                            No profit is generated from donations. Donating does not guarantee access to additional content nor its exclusivity if such content is ever given.
+                        </h2>
+                    </div>
+                </div>
+            </div>
+
+            <img className="relative z-0 h-120 object-cover w-screen opacity-5"
+                src={BackgroundImage} />
+        </section>
+    );
+};
