1f4c1a4832c768a8ef62d7136a72b2e16d1201f3
Erick Torres <30361843+ErickSharp@users.noreply.github.com>
2021-02-18 22:46:10 -0600

    refactor: move around code to try and comply with convention

--- src/components/utils/Error404.tsx
+++ src/components/utils/Error404.tsx
@@ -6,11 +6,11 @@ export const Error404 = () => {
     return (
         <>
             <div className="mx-auto my-80 max-w-md md:max-w-7xl text-center ">
-                <h5 className="font-bold text-9xl text-blue-light">404</h5>
-                <h2 className="font-bold text-4xl mt-12">
+                <h5 className="text-9xl text-blue-light font-bold">404</h5>
+                <h2 className="mt-12 text-4xl font-bold">
                     Oh no! It seems like the URL that you requested from us doesn&apos;t exist.
                 </h2>
-                <h1 className="font-medium text-xl text-grey-light mt-4">
+                <h1 className="mt-4 text-xl text-grey-light font-medium">
                     If you believe this was a mistake, reach out to us through our Discord,
                     or return to the homepage.
                 </h1>
