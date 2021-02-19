95084e11616a4ef47fd977f981244c40c2109018
Nathan Innes <nathaninnes001@gmail.com>
2021-02-19 22:18:58 +0000

    feat: theme colour on mobile

--- src/components/utils/NavBar.tsx
+++ src/components/utils/NavBar.tsx
@@ -21,7 +21,7 @@ export function NavBar(): JSX.Element {
     const [isOpen, setOpen] = useState(false);
 
     return (
-        <nav className={`fixed top-0 left-0 right-0 mx-auto py-2 z-50 ${scroll || isOpen ? 'transition bg-blue-dark-contrast shadow-lg-dark' : 'transition transparent'}`}>
+        <nav className={`fixed top-0 left-0 right-0 mx-auto py-2 z-50 ${scroll || isOpen ? 'transition bg-blue-dark-contrast shadow-lg-dark' : 'transition bg-blue-dark-contrast md:bg-transparent'}`}>
             <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="relative flex items-center justify-between h-16">
                     <Link to="/">
