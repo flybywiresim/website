3e7bbb0589c82da8417e0e22bd198ec81a10ef4c
Nathan Innes <nathaninnes001@gmail.com>
2021-02-19 22:56:35 +0000

    feat: add temp projects page content

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import bgVideo from '../../assets/video/A32NX.mp4';
 
-export const Projects = () => {
-    return (
-        <div className="mt-20 text-center">
-            <p className="font-bold text-3xl">
-                Test
+export const Projects: React.FC = () => (
+    <>
+        <div className="relative mx-auto px-2 my-80 max-w-md md:max-w-4xl text-center z-20">
+            <h1 className="text-8xl text-blue-light font-bold">Coming Soon</h1>
+            <p className="mt-4 pt-4 text-4xl font-bold">
+                    Content is coming soon to this page, check back later.
             </p>
-            <img src="https://hg1.funnyjunk.com/pictures/Sexy+duck_709139_6636765.jpg" alt="duck" className="2xl:w-56 m-auto"/>
         </div>
-    );
-};
+
+        <video className="absolute top-0 h-screen object-cover w-screen opacity-10"
+            src={bgVideo} playsInline autoPlay
+            muted loop />
+    </>
+);
