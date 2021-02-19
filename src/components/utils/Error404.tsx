aa95764919041c725262793f8c76345edbf2359d
Erick Torres <30361843+ErickSharp@users.noreply.github.com>
2021-02-19 10:20:08 -0600

    Update src/components/utils/Error404.tsx
    Co-authored-by: Nathan Innes <nathaninnes001@gmail.com>

--- src/components/utils/Error404.tsx
+++ src/components/utils/Error404.tsx
@@ -6,7 +6,7 @@ export const Error404 = () => {
         <>
             <div className="mx-auto my-80 max-w-md md:max-w-7xl text-center ">
                 <h5 className="text-9xl text-blue-light font-bold">404</h5>
-                <h2 className="mt-12 text-4xl font-bold">
+                <h2 className="mt-8 text-4xl font-bold">
                     Oh no! It seems like the URL that you requested from us doesn&apos;t exist.
                 </h2>
                 <h1 className="mt-4 text-xl text-grey-light font-medium">
