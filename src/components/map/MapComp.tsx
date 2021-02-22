8404e0baedaafba2e15936e960ed386d75483948
Erick Torres <30361843+ErickSharp@users.noreply.github.com>
2021-02-22 13:03:04 -0600

    Update src/components/map/MapComp.tsx
    Co-authored-by: Niklas Steiner <niklas@sbg.at>

--- src/components/map/MapComp.tsx
+++ src/components/map/MapComp.tsx
@@ -6,7 +6,7 @@ export const MapComp: React.FC = () => {
         <>
             <div className=" h-screen bg-blue-dark-contrast">
                 <div className="relative h-screen pt-20 text-black z-20">
-                    <Map refreshInterval={10_000} disableMenu={true} disableWeather={false} disableScroll={false} forceTileset={'carto-light'} />
+                    <Map />
                 </div>
             </div>
         </>
