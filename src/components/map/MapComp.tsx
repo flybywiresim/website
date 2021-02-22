d3ad6ea01d3a78d23f0799cac9f45955caa72b59
Erick Torres <30361843+ErickSharp@users.noreply.github.com>
2021-02-22 13:24:52 -0600

    feat: add map throttling logic from "Community"

--- src/components/map/MapComp.tsx
+++ src/components/map/MapComp.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Map } from '@flybywiresim/map';
 
 export const MapComp: React.FC = () => {
+    const [mapRefreshInterval, setMapRefreshInterval] = useState(5_000);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setMapRefreshInterval(document.visibilityState === 'visible' ? 10_000 : 100_000);
+        }, 100);
+
+        return () => clearInterval(interval);
+    }, []);
     return (
         <>
             <div className=" h-screen bg-blue-dark-contrast">
                 <div className="relative h-screen pt-20 text-black z-20">
-                    <Map />
+                    <Map refreshInterval={mapRefreshInterval} />
                 </div>
             </div>
         </>
