196480518cf61134f39fb50bdc64185e0f271947
benjozork <benjamin.dupont.984@gmail.com>
2021-02-23 20:37:59 -0500

    build: temporarily disable PurgeCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    purge: {
+        enabled: false,
+        content: [
+            './src/**/*.{js,jsx,ts,tsx}',
+            './public/index.html'
+        ]
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
