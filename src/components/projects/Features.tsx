765e169ec46179ed4008ad0fa62f3b71d3effe30
armankir <twrblx@gmail.com>
2021-03-08 09:52:54 -0500

    fix: mobile padding issues + grammar

--- src/components/projects/Features.tsx
+++ src/components/projects/Features.tsx
@@ -4,7 +4,7 @@ import { faCogs, faCheck, faEye } from '@fortawesome/free-solid-svg-icons';
 
 export const Features = forwardRef<HTMLDivElement>((_, ref): JSX.Element => (
     <>
-        <div className="mx-auto divide-y divide-gray-500 max-w-2xl mt-12">
+        <div className="mx-auto divide-y divide-gray-500 max-w-2xl mt-12 px-4">
             <p className="mt-3 mb-4 pt-3 text-5xl text-center font-medium text-blue-light">
                 Features
             </p>
@@ -13,12 +13,12 @@ export const Features = forwardRef<HTMLDivElement>((_, ref): JSX.Element => (
             </p>
         </div>
         <div id="features" className="bg-blue-darker bg-opacity-95 relative py-14">
-            <div className="rounded-lg mx-auto px-4 py-4 text-center sm:px-6 lg:px-0 lg:w-4/5 lg:max-w-7xl">
+            <div className="rounded-lg mx-auto px-4 py-4 text-center sm:px-6 lg:px-0 lg:max-w-7xl">
                 <div className="mt-12">
                     <div className="grid grid-cols-1 md:grid-cols-3 space-y-2 lg:justify-center divide-y md:divide-x md:divide-y-0 divide-gray-500">
                         <Card icon={faCogs}>
                             <CardTitle>Accurate System Functionality</CardTitle>
-                            <CardBody>Experimental version of the A32NX include limited to full rewrites of default autopilot and Fly-by-wire systems and much more.</CardBody>
+                            <CardBody>Experimental versions of the A32NX include full rewrites of default autopilot and Fly-by-wire systems and much more.</CardBody>
                         </Card>
                         <div className="pt-5 md:pt-0">
                             <Card icon={faEye}>
