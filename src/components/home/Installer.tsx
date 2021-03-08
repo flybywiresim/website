08146956b30f9dbb3bab7fe646e1fd2ad4d348ba
ErickSharp <titaninmc@gmail.com>
2021-03-07 19:10:37 -0600

    fix: issue with centering of left hand text section

--- src/components/home/Installer.tsx
+++ src/components/home/Installer.tsx
@@ -7,13 +7,13 @@ import InstallerImage from '../../assets/img/InstallerScreenshot.png';
 
 export const Installer: React.FC = () => {
     return (
-        <div className="relative h-144 w-screen flex flex-col xl:flex-row items-center overflow-hidden bg-gray-100 ">
-            <div className="ml-20  mt-10 max-w-2xl flex flex-col items-center xl:items-start text-center xl:text-left -mb-20 xl:mb-20 space-y-4">
+        <div className="relative h-144 w-screen flex flex-col xl:flex-row items-center overflow-hidden bg-blue-dark ">
+            <div className="mx-auto lg:ml-20 mt-10 max-w-2xl flex flex-col items-center xl:items-start text-center xl:text-left -mb-20 xl:mb-20 space-y-4">
                 <div className="inline-flex items-center justify-center p-3 bg-blue-light-contrast rounded-md shadow-lg">
                     <FontAwesomeIcon icon={faBoxOpen} size="2x" />
                 </div>
-                <h1 className="text-5xl pt-4 text-teal-light-contrast font-bold">Custom-built Installer</h1>
-                <h1 className="text-xl text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis delectus facilis et, totam architecto nisi.</h1>
+                <h1 className="text-5xl pt-4 text-white font-bold">Custom-built Installer</h1>
+                <h1 className="text-xl text-gray-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis delectus facilis et, totam architecto nisi.</h1>
                 <a href="https://api.flybywiresim.com/installer">
                     <Button className="w-40 mt-4 bg-teal-light-contrast border-2 border-teal-light-contrast hover:bg-white hover:text-teal-light-contrast  font-bold">Download</Button>
                 </a>
