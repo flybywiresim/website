368bd97bbc749a49ca2fbaae6f54aef14d30a6fe
Arman Kirakosyan <twrblx@gmail.com>
2021-03-10 13:08:18 -0500

    style: make projects text larger (#138)

--- src/components/projects/A32NX.tsx
+++ src/components/projects/A32NX.tsx
@@ -8,7 +8,7 @@ export const A32NX: React.FC = () => (
         <div className="absolute max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 inset-x-2 inset-y-1/3 z-30">
             <div className="flex flex-row justify-between items-end">
                 <div>
-                    <h2 className="text-xl font-medium tracking-widest text-white uppercase ml-2 mb-2">
+                    <h2 className="text-2xl font-medium tracking-widest text-white uppercase ml-2 mb-2">
                         DISCOVER THE
                     </h2>
                     <h1 className="text-7xl sm:text-8xl font-medium">
