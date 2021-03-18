dfc0cdc1cf1cb0599c970cb0050e6a1de36bba85
ErickSharp <titaninmc@gmail.com>
2021-03-18 14:25:02 -0500

    refactor: rename a32nx header, "Hero" to be less confusing

--- src/pages/projects/A32NX/A32NX.tsx
+++ src/pages/projects/A32NX/A32NX.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { Hero } from '../../../components/projects/A32NX/Hero';
+import { A32NXProjHeader } from '../../../components/projects/A32NX/A32NXProjHeader';
 import { Features } from '../../../components/projects/A32NX/Features';
 import { ExtendedFeatures } from '../../../components/projects/A32NX/ExtendedFeatures';
 import { Download } from '../../../components/projects/A32NX/Download';
 
 export const A32NX: React.FC = () => (
     <>
-        <Hero />
+        <A32NXProjHeader />
         <Features />
         <ExtendedFeatures />
         <Download />
