d6ac8a594b4e722f1b5618e3020460c991260907
Benjamin Dupont <benjozorkfr@gmail.com>
2021-04-05 19:21:49 -0400

    build: switch to fbw eslint config (#142)

--- src/components/utils/Card.tsx
+++ src/components/utils/Card.tsx
@@ -10,27 +10,25 @@ type CardProps = Partial<{
     icon: IconProp
 }>;
 
-export const CardTitle: React.FC<Props> = (props) => {
-    return (
-        <h3 className={`${props.className} pb-3 text-2xl text-blue-200 font-bold`}>{props.children}</h3>
-    );
-};
+export const CardTitle: React.FC<Props> = (props) => (
+    <h3 className={`${props.className} pb-3 text-2xl text-blue-200 font-bold`}>{props.children}</h3>
+);
 
-export const CardBody: React.FC<Props> = (props) => {
-    return (
-        <p className={props.className}>
-            {props.children}
-        </p>
-    );
-};
+export const CardBody: React.FC<Props> = (props) => (
+    <p className={props.className}>
+        {props.children}
+    </p>
+);
 
 export const Card: React.FC<CardProps> = (props): JSX.Element => (
     <div className="pt-6">
         <div
-            className="h-full flow-root transform hover:scale-105 transition-transform duration-100 rounded-lg px-8 pb-8">
+            className="h-full flow-root transform hover:scale-105 transition-transform duration-100 rounded-lg px-8 pb-8"
+        >
             <div className="-mt-6">
                 <span
-                    className="inline-flex items-center justify-center p-3 w-16 bg-blue-light-contrast rounded-md shadow-lg">
+                    className="inline-flex items-center justify-center p-3 w-16 bg-blue-light-contrast rounded-md shadow-lg"
+                >
                     {props.icon && <FontAwesomeIcon icon={props.icon} size="2x" />}
                 </span>
             </div>
