6b8b51e146d6cfd424adad871cf6ba6a4d91f33c
ErickSharp <titaninmc@gmail.com>
2021-04-05 14:10:55 -0500

    feat: add pagination so user can see what slide they are on

--- src/components/home/HeroHeader.tsx
+++ src/components/home/HeroHeader.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import SwiperCore, { Pagination } from 'swiper';
 import { Swiper } from 'swiper/react';
 import 'swiper/swiper.scss';
+import 'swiper/components/pagination/pagination.scss';
+
+SwiperCore.use([Pagination]);
 
 export const HeroHeader: React.FC = ({ children }) => {
     return (
         <>
-            <Swiper id="main">
+            <Swiper
+                spaceBetween={0}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+            >
                 {children}
             </Swiper>
         </>
