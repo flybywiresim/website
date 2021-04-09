629454c3cc384b8c8f3b89568b6a60f66765dc52
Erick Torres <30361843+ErickSharp@users.noreply.github.com>
2021-04-08 20:26:40 -0500

    refactor: update temporary installer photo (#151)

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,21 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
+            screens: {
+                '3xl': '1792px',
+                '4xl': '2048px',
+            },
+            spacing: {
+                '128': '32rem',
+                '160': '40rem',
+                '192': '48rem',
+                '224': '56rem',
+                '256': '64rem',
+                '288': '72rem',
+                '320': '80rem',
+                '352': '88rem',
+                '384': '96rem',
+            },
             colors: {
                 blue: {
                     'dark-contrast': '#1d2838'
