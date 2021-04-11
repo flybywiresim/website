25ce4648fb277ade72f43d977ed9521556dc4d8e
Benjamin Dupont <benjozorkfr@gmail.com>
2021-04-10 22:28:05 -0400

    fix: enable PurgeCSS (#160)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
+'use strict';
+
+const reactComponentsSafeList = require('@flybywiresim/react-components/build/usedCSSClasses.json');
+
 module.exports = {
     purge: {
-        enabled: false,
+        enabled: true,
+        options: { safelist: [...reactComponentsSafeList] },
         content: [
             './src/**/*.{js,jsx,ts,tsx}',
-            './public/index.html'
-        ]
+            './public/index.html',
+        ],
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
@@ -14,25 +19,21 @@ module.exports = {
                 '4xl': '2048px',
             },
             spacing: {
-                '128': '32rem',
-                '160': '40rem',
-                '192': '48rem',
-                '224': '56rem',
-                '256': '64rem',
-                '288': '72rem',
-                '320': '80rem',
-                '352': '88rem',
-                '384': '96rem',
+                128: '32rem',
+                160: '40rem',
+                192: '48rem',
+                224: '56rem',
+                256: '64rem',
+                288: '72rem',
+                320: '80rem',
+                352: '88rem',
+                384: '96rem',
             },
             colors: {
-                blue: {
-                    'dark-contrast': '#1d2838'
-                },
-                discord: {
-                    'DEFAULT': '#7289DA'
-                }
-            }
-        }
+                blue: { 'dark-contrast': '#1d2838' },
+                discord: { DEFAULT: '#7289DA' },
+            },
+        },
     },
     plugins: [require('@flybywiresim/tailwind-config')],
 };
