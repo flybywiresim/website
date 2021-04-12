3c582ff4454081b08d55e1f43f3c4f60da2332e7
GhostEagle68 <Ghosteagle68@gmail.com>
2021-04-12 16:48:41 -0600

    reafactor: update to tabler icons and tiny bit of feature page reorganization

--- src/components/home/Installer.tsx
+++ src/components/home/Installer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBoxOpen } from '@fortawesome/free-solid-svg-icons';
+import { IconPackage } from '@tabler/icons';
 import { Button } from '../Button';
 // TODO: Update image when installer redesign is finished.
 import InstallerImage from '../../assets/img/InstallerScreenshot.png';
@@ -9,7 +8,7 @@ export const Installer: React.FC = () => (
     <section id="installer" className="relative w-screen flex flex-col 3xl:flex-row 3xl:justify-center items-center py-6 overflow-hidden bg-blue-dark">
         <div className="mt-10 max-w-2xl flex flex-col items-center 3xl:items-start text-center 3xl:text-left 3xl:mb-20 3xl:mr-12 space-y-4">
             <div className="inline-flex items-center justify-center p-3 bg-blue-light-contrast rounded-md shadow-lg">
-                <FontAwesomeIcon icon={faBoxOpen} size="2x" />
+                <IconPackage size={50} stroke={1.5} />
             </div>
             <h1 className="text-5xl pt-4 text-white font-bold">
                 Custom-built Installer
--- src/components/projects/A32NX/ExtendedFeatures.tsx
+++ src/components/projects/A32NX/ExtendedFeatures.tsx
@@ -1,89 +1,126 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { IconHeadset, IconMap, IconPackage, IconVersions, IconReplace, IconFileText, IconBrandGithub, IconSquareCheck, IconEyeCheck, IconUserCheck, IconDeviceTablet } from '@tabler/icons';
 
 export const ExtendedFeatures: React.FC = () => (
     <section className="rounded-lg mx-auto px-4 py-4 max-w-screen-2xl sm:px-6 lg:px-8 pb-14">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 max-w-max mx-auto gap-x-14 gap-y-20 mt-10">
-            <Feature key="EFB">
+
+            <Feature key="Accurate System Functionality">
+                <IconSquareCheck size={40} stroke={1.5} color="teal" />
                 <FeatureTitle>
-                    Electronic Flight Bag
+                    Accurate System Functionality
                 </FeatureTitle>
                 <FeatureBody>
-                    The EFB is an integrated tablet with a handy toolkit, a settings page, performance tools, and more.
+                    The experimental version of the A32NX includes full rewrites of the default autopilot, Fly-by-wire system and much more.
                 </FeatureBody>
             </Feature>
 
             <Feature key="Custom Soundpack">
+                <IconHeadset size={40} stroke={1.5} color="teal" />
                 <FeatureTitle>
                     Custom Soundpack
                 </FeatureTitle>
                 <FeatureBody>
-                    Enjoy sounds like never before, the A32NX includes a fully custom and highly accurate soundpack.
-                </FeatureBody>
-            </Feature>
-
-            <Feature key="Simbrief Integration">
-                <FeatureTitle>
-                    Simbrief Integration
-                </FeatureTitle>
-                <FeatureBody>
-                    Make flight plan entry a breeze with the SimBrief integration, all it takes is one click and you are ready to go!
-                </FeatureBody>
-            </Feature>
-
-            <Feature key="Product Installer">
-                <FeatureTitle>
-                    Product Installer
-                </FeatureTitle>
-                <FeatureBody>
-                    FlyByWire has a custom product installer, which makes the task of installing all FlyByWire products hassle free.
-                </FeatureBody>
-            </Feature>
-
-            <Feature key="Experimental Variations">
-                <FeatureTitle>
-                    Experimental Variations
-                </FeatureTitle>
-                <FeatureBody>
-                    Want to experiment with our new features? You can! We have several ready to test branches with experimental features on our installer!
+                    The A32NX includes a fully custom and highly accurate sound-pack to enhance that little bit of realism.
                 </FeatureBody>
             </Feature>
 
             <Feature key="Constant Additions">
+                <IconReplace size={40} stroke={1.5} color="teal" />
                 <FeatureTitle>
                     Constant Additions
                 </FeatureTitle>
                 <FeatureBody>
-                    The A32NX development branches are updated almost daily with brand new features you can test right away!
+                    The A32NX development branches are updated almost daily with brand new features you can test right away and provide feedback on!
                 </FeatureBody>
             </Feature>
 
             <Feature key="Documentation">
+                <IconFileText size={40} stroke={1.5} color="teal" />
                 <FeatureTitle>
                     Documentation
                 </FeatureTitle>
                 <FeatureBody>
-                    FlyByWire has created several tutorials and documents to make learning the A320 quickly and accurately possible for anyone!
+                    The FlyByWire team has created several tutorials and documents to make learning the A320 quickly and accurately possible for anyone!
+                </FeatureBody>
+            </Feature>
+
+            <Feature key="Electronic Flight Bag">
+                <IconDeviceTablet size={40} stroke={1.5} color="teal" />
+                <FeatureTitle>
+                    Electronic Flight Bag
+                </FeatureTitle>
+                <FeatureBody>
+                    The FlyByWire team has created a custom EFB that comes with a handy toolkit, a settings page, performance tools, and much more!
+                </FeatureBody>
+            </Feature>
+
+            <Feature key="Experimental Variations">
+                <IconVersions size={40} stroke={1.5} color="teal" />
+                <FeatureTitle>
+                    Experimental Variations
+                </FeatureTitle>
+                <FeatureBody>
+                    Want to experiment with our new features? You can! We have several ready to test branches such as Stable, Development and experimental.
+                    The experimental version of the A32NX includes full rewrites of the default autopilot, Fly-by-wire system and much more.
                 </FeatureBody>
             </Feature>
 
             <Feature key="Open Source">
+                <IconBrandGithub size={40} stroke={1.5} color="teal" />
                 <FeatureTitle>
                     Open Source
                 </FeatureTitle>
                 <FeatureBody>
-                    The FlyByWire A32NX is completely open sourced, anyone can contribute to the project, and it can be freely reused under our licence.
+                    All the FlyByWire projects such as A32NX is completely open sourced which means anyone can contribute to the project, and it can be freely reused under our licence.
+                </FeatureBody>
+            </Feature>
+
+            <Feature key="Product Installer">
+                <IconPackage size={40} stroke={1.5} color="teal" />
+                <FeatureTitle>
+                    Product Installer
+                </FeatureTitle>
+                <FeatureBody>
+                    FlyByWire has a custom product installer which makes the task of installing all FlyByWire products hassle free.
+                </FeatureBody>
+            </Feature>
+
+            <Feature key="Simbrief Integration">
+                <IconMap size={40} stroke={1.5} color="teal" />
+                <FeatureTitle>
+                    Simbrief Integration
+                </FeatureTitle>
+                <FeatureBody>
+                    Make flight plan entry a breeze with the SimBrief integration. All it takes is one click and you are ready to go!
+                </FeatureBody>
+            </Feature>
+
+            <Feature key="Verified by Real Pilots">
+                <IconUserCheck size={40} stroke={1.5} color="teal" />
+                <FeatureTitle>
+                    Verified by Real Pilots
+                </FeatureTitle>
+                <FeatureBody>
+                    The A32NX has been analyzed and tested by hundreds of real A320 pilots for accuracy to the real life aircraft.
+                </FeatureBody>
+            </Feature>
+
+            <Feature key="Visual Fidelity">
+                <IconEyeCheck size={40} stroke={1.5} color="teal" />
+                <FeatureTitle>
+                    Visual Fidelity
+                </FeatureTitle>
+                <FeatureBody>
+                    The A32NX is packaged with multiple visual reworks including new lighting, reworked textures, and model additions with more to come!
                 </FeatureBody>
             </Feature>
         </div>
         <div id="download" />
     </section>
 );
-
 const Feature: React.FC = (props) => (
     <div className="w-80 flex flex-row">
-        <FontAwesomeIcon className="mb-auto mt-1 mr-4 text-green-500 text-2xl" size="1x" icon={faCheck} />
         <div className="space-y-3">
             {props.children}
         </div>
