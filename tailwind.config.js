e59fd3ad2d49b6ae6dfe6334afc5ef584a2ab330
benjozork <benjamin.dupont.984@gmail.com>
2021-04-18 21:01:30 -0400

    fix: re-exclude react-components tw classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         content: [
             './pages/**/*.{js,ts,jsx,tsx}',
             './components/**/*.{js,ts,jsx,tsx}',
+            './node_modules/@flybywiresim/react-components/build/usedCSSClasses.json',
         ],
     },
     darkMode: false, // or 'media' or 'class'
