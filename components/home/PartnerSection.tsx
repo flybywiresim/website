6b8d6300dbc8e626fb45507e1c97a0c31cfe505c
ErickSharp <titaninmc@gmail.com>
2021-04-22 16:46:57 -0500

    refactor: change gaps between cards and introduce even more linting headaches 😎

--- components/home/PartnerSection.tsx
+++ components/home/PartnerSection.tsx
@@ -11,7 +11,7 @@ export function PartnerSection(): JSX.Element {
                     Partners
                 </p>
             </div>
-            <div className="grid grid-cols sm:grid-cols-1 md:grid-cols-3 mt-6 mx-auto justify-center items-center gap-x-8 max-w-6xl">
+            <div className="grid grid-cols sm:grid-cols-1 md:grid-cols-3 gap-y-10 mt-10 mx-auto justify-center items-center gap-x-8 max-w-6xl">
                 <BoxCard>
                     <BoxImage className="w-60 pt-16 pb-3" src="img/partners/flightsimto.png" alt="Flightsim.to" />
                     <BoxBody path="https://flightsim.to/">
@@ -46,7 +46,7 @@ export function PartnerSection(): JSX.Element {
                     </BoxBody>
                 </BoxCard>
             </div>
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 md:mt-6 mx-auto justify-center items-center gap-x-8 max-w-6xl">
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-y-10 sm:mt-10 md:mt-8 mx-auto justify-center items-center gap-x-8 max-w-6xl">
                 <BoxCard>
                     <BoxImage className="w-20 pt-1 hover:scale-75" src="img/partners/salty.svg" alt="Salty Simulations" />
                     <BoxBody path="https://sim4flight.com/salty/">
