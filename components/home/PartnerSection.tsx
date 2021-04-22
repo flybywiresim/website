32b5c9404c9f17e7c6c16a931408e5dab61ba320
ErickSharp <titaninmc@gmail.com>
2021-04-22 16:33:21 -0500

    refactor: add image 💪

--- components/home/PartnerSection.tsx
+++ components/home/PartnerSection.tsx
@@ -3,14 +3,15 @@ import { BoxCard, BoxImage, BoxBody } from '../utils/BoxCard';
 
 export function PartnerSection(): JSX.Element {
     return (
-        <section className="relative w-full px-10 py-14">
+        <section className="relative overflow-hidden w-full px-10 py-14">
+            <img className="absolute h-384 -mt-20 -mx-12 w-full opacity-5" src="img/prev.jpg" />
             <div className="w-1/2 2xl:w-1/4 text-center mx-auto divide-y divide-gray-400">
                 <h2 className="text-base font-semibold tracking-wider text-blue-200 uppercase">OUR AFFILIATED</h2>
                 <p className="mt-3 pt-3 text-5xl font-extrabold text-blue-light">
                     Partners
                 </p>
             </div>
-            <div className="flex flex-row mt-6 mx-auto justify-center items-center gap-x-8 max-w-6xl">
+            <div className="grid grid-cols sm:grid-cols-1 md:grid-cols-3 mt-6 mx-auto justify-center items-center gap-x-8 max-w-6xl">
                 <BoxCard>
                     <BoxImage className="w-60 pt-16 pb-3" src="img/partners/flightsimto.png" alt="Flightsim.to" />
                     <BoxBody path="https://flightsim.to/">
@@ -45,7 +46,7 @@ export function PartnerSection(): JSX.Element {
                     </BoxBody>
                 </BoxCard>
             </div>
-            <div className="flex flex-row mt-6 mx-auto justify-center items-center gap-x-8 max-w-6xl">
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 md:mt-6 mx-auto justify-center items-center gap-x-8 max-w-6xl">
                 <BoxCard>
                     <BoxImage className="w-20 pt-1 hover:scale-75" src="img/partners/salty.svg" alt="Salty Simulations" />
                     <BoxBody path="https://sim4flight.com/salty/">
