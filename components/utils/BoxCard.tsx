ff5c3b5255702f090a197149fe64a1b74654ec3a
ErickSharp <titaninmc@gmail.com>
2021-04-22 02:09:38 -0500

    feat: add Learn More button 😎

--- components/utils/BoxCard.tsx
+++ components/utils/BoxCard.tsx
@@ -1,9 +1,14 @@
 import React, { PropsWithChildren } from 'react';
+import { Button } from './Button';
 
 type ImageProps = {
     className?: string,
     src: string,
     alt?: string
+};
+
+type BodyProps = {
+    path: string
 };
 
 export const BoxImage: React.FC<ImageProps> = (props: PropsWithChildren<ImageProps>) => (
@@ -13,14 +18,17 @@ export const BoxImage: React.FC<ImageProps> = (props: PropsWithChildren<ImagePro
 /**
  * Max recommended body size: 300 characters
  */
-export const BoxBody: React.FC = ({ children }) => (
+export const BoxBody: React.FC<BodyProps> = (props: PropsWithChildren<BodyProps>) => (
     <div className="px-8 py-8 max-w-xl text-justify">
-        {children}
+        {props.children}
+        <a href={props.path} target="_blank" rel="noreferrer">
+            <Button className="w-full mt-4 bg-teal-light-contrast border-2 border-teal-light-contrast hover:bg-white hover:text-teal-light-contrast font-bold">Learn More</Button>
+        </a>
     </div>
 );
 
 export const BoxCard: React.FC = ({ children }) => (
-    <div className="max-w-full h-full transform hover:scale-105 transition-transform bg-blue-dark-contrast shadow-xl hover:shadow-2xl rounded-md">
+    <div className="max-w-full h-full transform hover:scale-105 transition-transform duration-200 bg-blue-dark-contrast shadow-xl hover:shadow-2xl rounded-md">
         {children}
     </div>
 );
