f072bfed6f034b4a197b28a1893d39000c80f9e9
Nathan Innes <nathaninnes001@gmail.com>
2021-04-25 18:43:42 +0100

    refactor(footer): clean up and add higher resolution image

--- components/utils/Footer.tsx
+++ components/utils/Footer.tsx
@@ -49,17 +49,19 @@ export const Footer = (): JSX.Element => (
                             </Link>
                         </div>
                     </div>
-                    <div>
+
+                    <div className="mt-2">
                         <a
-                            className="hover:underline flex flex-row mx-auto mt-2 justify-center"
+                            className="flex justify-center"
                             href="https://vercel.com/?utm_source=[flybywiresim]&utm_campaign=oss"
                             target="_blank"
                             rel="noreferrer"
                         >
-                            <p>Powered by</p>
-                            <img className="pl-2 h-5" src="img/Vercel-Logotype-Light.png" alt="Vercel" />
+                            Powered by
+                            <img className="mx-2 h-4 self-center" src="svg/vercel.svg" alt="Vercel" />
                         </a>
                     </div>
+
                     <p className="text-gray-400 pt-2">&copy; FlyByWire Simulations and its contributors 2020-2021</p>
                     <p className="text-sm text-gray-500 pt-2">
                         v
