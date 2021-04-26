f431540b9dc5fec765d3d2ec195b7e3d30728419
Nathan Innes <nathaninnes001@gmail.com>
2021-04-26 17:50:27 +0100

    feat: add card component

--- components/utils/Card.tsx
+++ components/utils/Card.tsx
@@ -1,40 +1,19 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import React, { FC } from 'react';
 
-type Props = Partial<{
-    className: string
-}>;
-
-type CardProps = Partial<{
-    icon: IconProp
-}>;
-
-export const CardTitle: React.FC<Props> = (props) => (
-    <h3 className={`${props.className} pb-3 text-2xl text-blue-200 font-bold`}>{props.children}</h3>
+export const Card: FC = (props) => (
+    <>
+        <div className="bg-gray-200 border-t-4 border-discord px-page py-6 max-w-2xl rounded-lg">
+            {props.children}
+        </div>
+    </>
 );
 
-export const CardBody: React.FC<Props> = (props) => (
-    <p className={props.className}>
+export const CardTitle: FC = (props) => (
+    <h1 className="text-4xl font-bold">{props.children}</h1>
+);
+
+export const CardBody: FC = (props) => (
+    <p className="mt-4 mb-6">
         {props.children}
     </p>
-);
-
-export const Card: React.FC<CardProps> = (props): JSX.Element => (
-    <div className="pt-6">
-        <div
-            className="h-full flow-root transform hover:scale-105 transition-transform duration-100 rounded-lg px-8 pb-8"
-        >
-            <div className="-mt-6">
-                <span
-                    className="inline-flex items-center justify-center p-3 w-16 bg-blue-light-contrast rounded-md shadow-lg"
-                >
-                    {props.icon && <FontAwesomeIcon icon={props.icon} size="2x" />}
-                </span>
-            </div>
-            <div className="mt-3">
-                {props.children}
-            </div>
-        </div>
-    </div>
 );
