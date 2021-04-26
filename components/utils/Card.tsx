4176e0dd642c2877bba370fdb9ce05496c3e650d
Nathan Innes <nathaninnes001@gmail.com>
2021-04-27 00:36:08 +0100

    feat(card): add more customisation options

--- components/utils/Card.tsx
+++ components/utils/Card.tsx
@@ -1,19 +1,22 @@
 import React, { FC } from 'react';
 
-export const Card: FC = (props) => (
-    <>
-        <div className="bg-gray-200 border-t-4 border-discord px-page py-6 max-w-2xl rounded-lg">
-            {props.children}
-        </div>
-    </>
-);
-
-export const CardTitle: FC = (props) => (
-    <h1 className="text-4xl font-bold">{props.children}</h1>
-);
-
-export const CardBody: FC = (props) => (
-    <p className="mt-4 mb-6">
+type cardProps = {
+    className?: string,
+    bgColour: string,
+    borderColour: string
+}
+export const Card: FC<cardProps> = (props) => (
+    <div className={`bg-${props.bgColour} border-t-4 border-${props.borderColour} px-page py-6 max-w-2xl rounded-lg ${props.className}`}>
         {props.children}
+    </div>
+);
+
+export const CardTitle: FC = ({ children }) => (
+    <h1 className="text-4xl font-bold">{children}</h1>
+);
+
+export const CardBody: FC = ({ children }) => (
+    <p className="mt-4 mb-6">
+        {children}
     </p>
 );
