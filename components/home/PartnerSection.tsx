30e5c2a5fe3552c256e89b2de188b5418c36e778
ErickSharp <titaninmc@gmail.com>
2021-04-26 21:15:14 -0500

    refactor(partners): change layout of the partners section 🦍

--- components/home/PartnerSection.tsx
+++ components/home/PartnerSection.tsx
@@ -13,7 +13,7 @@ type PartnerProps = Partial<{
 
 export const PartnerImage: React.FC<ImageProps> = (props: PropsWithChildren<ImageProps>) => (
     <div className={`p-8 h-full ${props.className}`}>
-        <img className="h-full w-full object-contain transform hover:scale-105 transition-transform duration-100 filter-grayscale hover:filter-none" src={props.src} alt="" />
+        <img className="h-full w-full object-contain transform hover:scale-105 transition-transform duration-100" src={props.src} alt="" />
     </div>
 );
 
@@ -27,31 +27,35 @@ export const Partner: React.FC<PartnerProps> = (props: PropsWithChildren<Partner
 
 export function PartnerSection(): JSX.Element {
     return (
-        <section className="relative w-full px-10 py-14">
-            <div className="w-1/2 2xl:w-1/4 text-center mx-auto divide-y divide-gray-400">
-                <h2 className="text-base font-semibold tracking-wider text-blue-200 uppercase">OUR AFFILIATED</h2>
-                <p className="mt-3 pt-3 text-5xl font-extrabold text-blue-light">
-                    Partners
-                </p>
-            </div>
-            <div className="w-1/2 sm:w-3/5 2xl:w-1/2 grid grid-cols-1 lg:grid-cols-3 mt-8 mx-auto">
-                <Partner name="Flightsim.to" path="https://flightsim.to/">
-                    <PartnerImage src="img/partners/flightsimto.png" />
-                </Partner>
-                <Partner name="FSNews" path="https://fsnews.eu/">
-                    <PartnerImage src="img/partners/fsnews.png" />
-                </Partner>
-                <Partner name="YourControls" path="https://github.com/Sequal32/yourcontrols">
-                    <PartnerImage src="img/partners/yourcontrols.png" />
-                </Partner>
-            </div>
-            <div className="w-1/2 sm:w-2/6 2xl:w-1/5 grid grid-cols-1 lg:grid-cols-2 mx-auto">
-                <Partner name="SaltySimulations" path="https://sim4flight.com/salty/">
-                    <PartnerImage className="w-2/3 mx-auto" src="img/partners/salty.svg" />
-                </Partner>
-                <Partner name="sim4flight" path="https://sim4flight.com/">
-                    <PartnerImage className="mx-auto" src="img/partners/s4f.png" />
-                </Partner>
+        <section className="relative w-full sm:py-20 lg:py-8">
+            <div className="h-full grid md:grid-cols-1 lg:grid-cols-5 items-center">
+                <div>
+                    <a href="https://flightsim.to/" target="_blank" rel="noreferrer">
+                        <img className="w-1/2 mx-auto" src="img/partners/flightsimto.png" alt="Flightsim.to" />
+                    </a>
+                </div>
+                <div className="sm:mt-16 lg:mt-0">
+                    <a href="https://fsnews.eu/" target="_blank" rel="noreferrer">
+                        <img className="w-1/2 mx-auto" src="img/partners/fsnews.png" alt="FSNews" />
+                    </a>
+                </div>
+                <div className="sm:mt-16 lg:mt-0">
+                    <a href="https://github.com/Sequal32/yourcontrols" target="_blank" rel="noreferrer">
+                        <img className="w-1/2 mx-auto" src="img/partners/yourcontrols.png" alt="YourControls" />
+                    </a>
+                </div>
+                <div className="sm:mt-16 lg:mt-0">
+                    <a className="flex items-center" href="https://sim4flight.com/salty/" target="_blank" rel="noreferrer">
+                        <img className="w-1/12 ml-auto" src="img/partners/salty.svg" alt="Salty Simulations" />
+                        <p className="font-bold lg:text-xl sm:text-6xl pl-2 mr-auto">Salty Simulations</p>
+                    </a>
+                </div>
+                <div className="sm:mt-16 lg:mt-0">
+                    <a className="flex items-center" href="https://sim4flight.com/" target="_blank" rel="noreferrer">
+                        <img className="w-1/12 ml-auto" src="img/partners/s4f.png" alt="Sim4flight" />
+                        <p className="font-bold lg:text-xl sm:text-6xl pl-2 mr-auto">Sim4flight</p>
+                    </a>
+                </div>
             </div>
         </section>
     );
