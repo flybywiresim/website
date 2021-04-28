42b6944d97b46a59623bcc5b6b8a931dd3ffc547
ErickSharp <titaninmc@gmail.com>
2021-04-28 05:22:52 -0500

    refactor: apply mx-auto to all PartnerImage elements and remove 'imageClassName' prop

--- components/home/PartnerSection.tsx
+++ components/home/PartnerSection.tsx
@@ -2,7 +2,6 @@ import React, { PropsWithChildren } from 'react';
 
 type ImageProps = {
     className?: string,
-    imgClassName?: string,
     src: string
 };
 
@@ -13,8 +12,8 @@ type PartnerProps = Partial<{
 }>;
 
 export const PartnerImage: React.FC<ImageProps> = (props: PropsWithChildren<ImageProps>) => (
-    <div className={props.className}>
-        <img className={props.imgClassName} src={props.src} alt="" />
+    <div className={`mx-auto ${props.className}`}>
+        <img src={props.src} alt="" />
     </div>
 );
 
@@ -28,22 +27,22 @@ export const Partner: React.FC<PartnerProps> = (props: PropsWithChildren<Partner
 
 export function PartnerSection(): JSX.Element {
     return (
-        <section className="w-full py-20 lg:py-8 ">
+        <section className="w-full py-20 lg:py-8">
             <div className="max-w-screen-2xl grid md:grid-cols-1 lg:grid-cols-5 space-y-16 lg:space-y-0 items-center mx-auto">
                 <Partner name="Flightsim.to" path="https://flightsim.to/">
-                    <PartnerImage className="w-1/2 mx-auto" src="img/partners/flightsimto.png" />
+                    <PartnerImage className="w-1/2" src="img/partners/flightsimto.png" />
                 </Partner>
                 <Partner name="FSNews" path="https://fsnews.eu/">
-                    <PartnerImage className="w-1/2 mx-auto" src="img/partners/fsnews.png" />
+                    <PartnerImage className="w-1/2" src="img/partners/fsnews.png" />
                 </Partner>
                 <Partner name="YourControls" path="https://github.com/Sequal32/yourcontrols">
-                    <PartnerImage className="w-1/2 mx-auto" src="img/partners/yourcontrols.png" />
+                    <PartnerImage className="w-1/2" src="img/partners/yourcontrols.png" />
                 </Partner>
                 <Partner name="SaltySimulations" path="https://sim4flight.com/salty/">
-                    <PartnerImage className="w-3/5 mx-auto" src="img/partners/salty.svg" />
+                    <PartnerImage className="w-3/5" src="img/partners/salty.svg" />
                 </Partner>
                 <Partner name="sim4flight" path="https://sim4flight.com/">
-                    <PartnerImage className="w-1/2 mx-auto" src="img/partners/s4f.png" />
+                    <PartnerImage className="w-1/2" src="img/partners/s4f.png" />
                 </Partner>
             </div>
         </section>
