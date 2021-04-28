97d28ce4ff4d549c5a7cb3103773b794ea6c8451
Nathan Innes <nathaninnes001@gmail.com>
2021-04-28 12:37:47 +0100

    fix(card): remove borderColour type

--- components/utils/Card.tsx
+++ components/utils/Card.tsx
@@ -3,7 +3,6 @@ import React, { FC } from 'react';
 type cardProps = {
     className?: string,
     bgColour: string,
-    borderColour: string
 }
 export const Card: FC<cardProps> = (props) => (
     <div className={`bg-${props.bgColour} border-t-4 px-page py-6 max-w-2xl rounded-lg ${props.className}`}>
