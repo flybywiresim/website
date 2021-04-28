7a934782bfa00388bc56de6314f50e47c5a38c1d
Erick Torres <30361843+ErickSharp@users.noreply.github.com>
2021-04-28 05:47:56 -0500

    refactor(partners): change layout of the partners section (#195)
    * refactor(partners): change layout of the partners section 🦍
    
    * refactor: remove h-full
    
    * refactor: put everything back into the partners components and add new props
    
    * fix: scaling issues
    
    * refactor: add text to svg for salty
    
    * refactor: remove text from s4f and replace with placeholder png
    
    * refactor: remove aTagClassname prop
    
    * fix: remove hover scaling css props to get rid of flickering when hovering
    
    * refactor: apply mx-auto to all PartnerImage elements and remove 'imageClassName' prop
    
    Co-authored-by: Nathan Innes <nathaninnes001@gmail.com>

--- components/home/PartnerSection.tsx
+++ components/home/PartnerSection.tsx
@@ -12,13 +12,13 @@ type PartnerProps = Partial<{
 }>;
 
 export const PartnerImage: React.FC<ImageProps> = (props: PropsWithChildren<ImageProps>) => (
-    <div className={`p-8 h-full ${props.className}`}>
-        <img className="h-full w-full object-contain transform hover:scale-105 transition-transform duration-100 filter-grayscale hover:filter-none" src={props.src} alt="" />
+    <div className={`mx-auto ${props.className}`}>
+        <img src={props.src} alt="" />
     </div>
 );
 
 export const Partner: React.FC<PartnerProps> = (props: PropsWithChildren<PartnerProps>) => (
-    <div id={props.name} className={`flex flex-col h-full justify-center ${props.className}`}>
+    <div id={props.name} className={`flex flex-col justify-center ${props.className}`}>
         <a href={props.path} target="_blank" rel="noreferrer">
             {props.children}
         </a>
@@ -27,30 +27,22 @@ export const Partner: React.FC<PartnerProps> = (props: PropsWithChildren<Partner
 
 export function PartnerSection(): JSX.Element {
     return (
-        <section className="relative w-full px-10 py-14">
-            <div className="w-1/2 2xl:w-1/4 text-center mx-auto divide-y divide-gray-400">
-                <h2 className="text-base font-semibold tracking-wider text-blue-200 uppercase">OUR AFFILIATED</h2>
-                <p className="mt-3 pt-3 text-5xl font-extrabold text-blue-light">
-                    Partners
-                </p>
-            </div>
-            <div className="w-1/2 sm:w-3/5 2xl:w-1/2 grid grid-cols-1 lg:grid-cols-3 mt-8 mx-auto">
+        <section className="w-full py-20 lg:py-8">
+            <div className="max-w-screen-2xl grid md:grid-cols-1 lg:grid-cols-5 space-y-16 lg:space-y-0 items-center mx-auto">
                 <Partner name="Flightsim.to" path="https://flightsim.to/">
-                    <PartnerImage src="img/partners/flightsimto.png" />
+                    <PartnerImage className="w-1/2" src="img/partners/flightsimto.png" />
                 </Partner>
                 <Partner name="FSNews" path="https://fsnews.eu/">
-                    <PartnerImage src="img/partners/fsnews.png" />
+                    <PartnerImage className="w-1/2" src="img/partners/fsnews.png" />
                 </Partner>
                 <Partner name="YourControls" path="https://github.com/Sequal32/yourcontrols">
-                    <PartnerImage src="img/partners/yourcontrols.png" />
+                    <PartnerImage className="w-1/2" src="img/partners/yourcontrols.png" />
                 </Partner>
-            </div>
-            <div className="w-1/2 sm:w-2/6 2xl:w-1/5 grid grid-cols-1 lg:grid-cols-2 mx-auto">
                 <Partner name="SaltySimulations" path="https://sim4flight.com/salty/">
-                    <PartnerImage className="w-2/3 mx-auto" src="img/partners/salty.svg" />
+                    <PartnerImage className="w-3/5" src="img/partners/salty.svg" />
                 </Partner>
                 <Partner name="sim4flight" path="https://sim4flight.com/">
-                    <PartnerImage className="mx-auto" src="img/partners/s4f.png" />
+                    <PartnerImage className="w-1/2" src="img/partners/s4f.png" />
                 </Partner>
             </div>
         </section>
