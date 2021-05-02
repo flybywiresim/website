28aac845eb4cde60cd161e49c4d7256658842347
Nathan Innes <nathaninnes001@gmail.com>
2021-05-02 19:15:23 +0100

    refactor(hero): parallax effect on video

--- components/home/headers/A320Header.tsx
+++ components/home/headers/A320Header.tsx
@@ -40,7 +40,7 @@ export const A320Header: React.FC = () => {
                 </div>
 
                 <video
-                    className="absolute top-0 h-screen object-cover w-screen opacity-10"
+                    className="fixed top-0 h-screen object-cover w-screen opacity-10 -z-10"
                     src="video/A32NX.mp4"
                     playsInline
                     autoPlay
