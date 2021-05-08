0c3e7bed8927bc980d9a1d462d2097efdd29af06
Erick Torres <30361843+ErickSharp@users.noreply.github.com>
2021-05-08 17:31:03 -0500

    refactor: fix mobile scaling COMPLETELY

--- pages/notams/[id].tsx
+++ pages/notams/[id].tsx
@@ -8,21 +8,21 @@ export type PostProps = { content: PostContent }
 const Post: React.FC<PostProps> = ({ content: { authors, contentHtml, date, readingStats, title } }) => (
     <>
         {/* This covers the background so it looks like the navbar is solid */}
-        <div className="h-20 bg-blue-dark-contrast" />
         <div className="min-h-screen w-screen mx-auto bg-cool pb-6">
-            <div className="h-144 bg-blue-dark overflow-hidden">
+            <div className="h-160 bg-blue-dark overflow-hidden">
                 <img
                     draggable="false"
-                    className="filter opacity-30 blur-sm brightness-90 w-screen -mt-4 h-160 object-cover"
+                    className="absolute filter opacity-30 blur-sm brightness-90 w-screen -mt-4 h-160 object-cover"
                     src="https://i.imgur.com/TFWckR1.png"
                     alt=""
                 />
-                <div className="absolute max-w-6xl lg:mx-20 px-page inset-x-2 inset-y-1/3 z-30">
+                <div className="flex flex-col relative h-160 justify-end pb-20 pt-20 max-w-6xl lg:mx-20 px-page z-30">
                     <div className="flex flex-row items-center bottom-0 gap-x-4 text-xl">
                         <p className="text-teal-light font-black">ANNOUNCEMENTS</p>
                         <span className="text-gray-300 font-medium">
                             {readingStats.text}
                         </span>
+
                     </div>
                     <h1 className="text-6xl font-semibold leading-tight text-white mt-0.5">
                         {title}
@@ -41,7 +41,7 @@ const Post: React.FC<PostProps> = ({ content: { authors, contentHtml, date, read
             </div>
             <div className="px-page mx-auto mt-20 max-w-6xl">
                 <div
-                    className="flex py-12 rounded-sm flex-col mx-auto space-y-4 text-xl text-black prose-2xl prose text-justify"
+                    className="flex rounded-sm flex-col mx-auto px-10 lg:px-0 space-y-4 text-xl text-black prose-2xl prose text-justify"
                     /* eslint-disable-next-line react/no-danger */
                     dangerouslySetInnerHTML={{ __html: contentHtml }}
                 />
