c5d782f826367216a40fc3d99185f53865b57178
Erick Torres <30361843+ErickSharp@users.noreply.github.com>
2021-05-08 14:13:29 -0500

    refactor: remove back button and move author and date

--- pages/notams/[id].tsx
+++ pages/notams/[id].tsx
@@ -3,27 +3,6 @@ import Link from 'next/link';
 import React from 'react';
 import { getAllPostIds, getPostContent, PostContent } from '../../lib/notams/posts';
 
-// Shamelessly stolen from tabler until the icons are updated
-const BackIcon: React.FC<{ className: string }> = ({ className }) => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className={`icon icon-tabler icon-tabler-arrow-narrow-left ${className}`}
-        width="32"
-        height="32"
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke="currentColor"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-    >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <line x1="5" y1="12" x2="19" y2="12" />
-        <line x1="5" y1="12" x2="9" y2="16" />
-        <line x1="5" y1="12" x2="9" y2="8" />
-    </svg>
-);
-
 export type PostProps = { content: PostContent }
 
 const Post: React.FC<PostProps> = ({ content: { authors, contentHtml, date, readingStats, title } }) => (
@@ -45,31 +24,22 @@ const Post: React.FC<PostProps> = ({ content: { authors, contentHtml, date, read
                             {readingStats.text}
                         </span>
                     </div>
-                    <h1 className="text-6xl font-semibold lg:text-justify leading-tight text-white mt-0.5 mb-20">
+                    <h1 className="text-6xl font-semibold lg:text-justify leading-tight text-white mt-0.5">
                         {title}
                     </h1>
+                    <div className="mb-20">
+                        <p>
+                            {'Written by '}
+                            {authors.join(', ')}
+                        </p>
+                        <p>
+                            {'Posted: '}
+                            {`${date.substring(0, 4)}/${date.substring(5, 7)}/${date.substring(8)}`}
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div className="px-page mx-auto mt-10 max-w-6xl">
-                <div className="text-center text-xl text-black">
-                    <p className="font-medium">
-                        {`${date.substring(5, 7)}/${date.substring(8)}/${date.substring(0, 4)}`}
-                    </p>
-                    <p className="font-medium">
-                        by
-                        {' '}
-                        {authors.join(', ')}
-                    </p>
-                </div>
-                <div className="w-60 h-2 mx-auto mt-10 mb-10 bg-teal-light-contrast rounded-full mb-3" />
-                <Link href="/notams">
-                    <div className="flex flex-row items-center cursor-pointer justify-center text-gray-400 hover:text-teal-700 transition-colors duration-100">
-                        <BackIcon className="-ml-2.5" />
-                        <span className="text-2xl ml-1.5">
-                            Back
-                        </span>
-                    </div>
-                </Link>
+            <div className="px-page mx-auto mt-20 max-w-6xl">
                 <div className="mt-7 px-6 py-4 text-xl bg-cool rounded-sm">
                     <div
                         className="flex flex-col space-y-4 text-black  prose-2xl prose text-justify"
