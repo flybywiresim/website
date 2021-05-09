02e13c2e622f0a4ea2ae92db07b7c432ace6b996
Nathan Innes <nathaninnes001@gmail.com>
2021-05-09 16:38:00 +0100

    refactor: remove unnecessary fragments

--- pages/notams/[id].tsx
+++ pages/notams/[id].tsx
@@ -5,59 +5,57 @@ import { getAllPostIds, getPostContent, PostContent } from '../../lib/notams/pos
 export type PostProps = { content: PostContent }
 
 const Post: React.FC<PostProps> = ({ content: { authors, category, contentHtml, date, readingStats, title, metaImage, metaAlt } }) => (
-    <>
-        <div className="min-h-screen w-full mx-auto bg-cool pb-6">
-            <div className="h-160 bg-blue-dark overflow-hidden shadow-md">
-                <img
-                    draggable="false"
-                    className="absolute filter blur-sm opacity-20 w-screen h-160 object-cover"
-                    src={metaImage}
-                    alt={metaAlt}
-                />
-                <div className="flex flex-col relative h-160 max-w-6xl 2xl:inset-x-80 justify-end pb-8 lg:pb-14 px-page">
-                    <div className="flex flex-row items-center text-xl">
-                        {category === 'ANNOUNCEMENTS'
-                            ? (
-                                <p
-                                    className="text-teal-light font-black"
-                                >
-                                    {category}
-                                </p>
-                            )
-                            : (
-                                <p
-                                    className="text-orangutanOrange font-black"
-                                >
-                                    {category}
-                                </p>
-                            )}
-                        <span className="text-gray-300 pl-4 font-medium">
-                            {readingStats.text}
-                        </span>
-                    </div>
-                    <h1 className="text-6xl font-semibold">
-                        {title}
-                    </h1>
-                    <div className="text-gray-300 pt-1">
-                        <p>
-                            {'Written by '}
-                            {authors.join(', ')}
-                        </p>
-                        <p>
-                            {'Posted: '}
-                            {`${date.substring(0, 4)}/${date.substring(5, 7)}/${date.substring(8)}`}
-                        </p>
-                    </div>
+    <div className="min-h-screen w-full mx-auto bg-cool pb-6">
+        <div className="h-160 bg-blue-dark overflow-hidden shadow-md">
+            <img
+                draggable="false"
+                className="absolute filter blur-sm opacity-20 w-screen h-160 object-cover"
+                src={metaImage}
+                alt={metaAlt}
+            />
+            <div className="flex flex-col relative h-160 max-w-6xl 2xl:inset-x-80 justify-end pb-8 lg:pb-14 px-page">
+                <div className="flex flex-row items-center text-xl">
+                    {category === 'ANNOUNCEMENTS'
+                        ? (
+                            <p
+                                className="text-teal-light font-black"
+                            >
+                                {category}
+                            </p>
+                        )
+                        : (
+                            <p
+                                className="text-orangutanOrange font-black"
+                            >
+                                {category}
+                            </p>
+                        )}
+                    <span className="text-gray-300 pl-4 font-medium">
+                        {readingStats.text}
+                    </span>
+                </div>
+                <h1 className="text-6xl font-semibold">
+                    {title}
+                </h1>
+                <div className="text-gray-300 pt-1">
+                    <p>
+                        {'Written by '}
+                        {authors.join(', ')}
+                    </p>
+                    <p>
+                        {'Posted: '}
+                        {`${date.substring(0, 4)}/${date.substring(5, 7)}/${date.substring(8)}`}
+                    </p>
                 </div>
             </div>
-
-            <article
-                className="flex flex-col mx-auto px-page mt-12 max-w-6xl space-y-4 text-xl text-black prose-2xl prose text-justify"
-                /* eslint-disable-next-line react/no-danger */
-                dangerouslySetInnerHTML={{ __html: contentHtml }}
-            />
         </div>
-    </>
+
+        <article
+            className="flex flex-col mx-auto px-page mt-12 max-w-6xl space-y-4 text-xl text-black prose-2xl prose text-justify"
+            /* eslint-disable-next-line react/no-danger */
+            dangerouslySetInnerHTML={{ __html: contentHtml }}
+        />
+    </div>
 );
 
 export const getStaticPaths: GetStaticPaths = async () => {
--- pages/notams/index.tsx
+++ pages/notams/index.tsx
@@ -6,21 +6,20 @@ import { getPostListings, PostListing } from '../../lib/notams/posts';
 export type BlogProps = { listings: PostListing[] }
 
 const Blog: React.FC<BlogProps> = ({ listings }) => (
-    <>
-        <div className="min-h-screen max-w-4xl lg:max-w-6xl mx-auto mb-8 px-page pt-28 lg:pt-20">
-            {listings.map(({
-                id,
-                date,
-                title,
-                authors,
-                readingStats,
-                category,
-                metaImage,
-                metaAlt,
-            }) => (
-                <Link href={`/notams/${id}`}>
-                    <ul key={id}>
-                        <div className="
+    <div className="min-h-screen max-w-4xl lg:max-w-6xl mx-auto mb-8 px-page pt-28 lg:pt-20">
+        {listings.map(({
+            id,
+            date,
+            title,
+            authors,
+            readingStats,
+            category,
+            metaImage,
+            metaAlt,
+        }) => (
+            <Link href={`/notams/${id}`}>
+                <ul key={id}>
+                    <div className="
                                 w-full
                                 my-12
                                 bg-white
@@ -32,59 +31,58 @@ const Blog: React.FC<BlogProps> = ({ listings }) => (
                                 ring-teal
                                 overflow-hidden
                                 cursor-pointer"
-                        >
-                            {metaImage
-                                ? (
-                                    <img
-                                        className="mb-2 max-h-80 object-cover"
-                                        src={metaImage}
-                                        alt={metaAlt}
-                                    />
-                                )
-                                : <></>}
-                            <div className="px-7 py-4">
-                                <div className="text-lg flex">
-                                    {category === 'ANNOUNCEMENTS'
-                                        ? (
-                                            <p
-                                                className="text-teal-light font-black"
-                                            >
-                                                {category}
-                                            </p>
-                                        )
-                                        : (
-                                            <p
-                                                className="text-orangutanOrange font-black"
-                                            >
-                                                {category}
-                                            </p>
-                                        )}
-                                    <span className="text-gray-700 font-medium pl-2">
-                                        {readingStats.text}
-                                    </span>
-                                </div>
-                                <h1 className="text-5xl text-black font-bold leading-tight">
-                                    {title}
-                                </h1>
-                                <div className="flex flex-col text-gray-700">
-                                    <p className="font-medium">
-                                        Written by
-                                        {' '}
-                                        {authors.join(', ')}
-                                    </p>
-                                    <p className="font-medium">
-                                        Posted:
-                                        {' '}
-                                        {`${date.substring(0, 4)}/${date.substring(5, 7)}/${date.substring(8)}`}
-                                    </p>
-                                </div>
+                    >
+                        {metaImage
+                            ? (
+                                <img
+                                    className="mb-2 max-h-80 object-cover"
+                                    src={metaImage}
+                                    alt={metaAlt}
+                                />
+                            )
+                            : <></>}
+                        <div className="px-7 py-4">
+                            <div className="text-lg flex">
+                                {category === 'ANNOUNCEMENTS'
+                                    ? (
+                                        <p
+                                            className="text-teal-light font-black"
+                                        >
+                                            {category}
+                                        </p>
+                                    )
+                                    : (
+                                        <p
+                                            className="text-orangutanOrange font-black"
+                                        >
+                                            {category}
+                                        </p>
+                                    )}
+                                <span className="text-gray-700 font-medium pl-2">
+                                    {readingStats.text}
+                                </span>
+                            </div>
+                            <h1 className="text-5xl text-black font-bold leading-tight">
+                                {title}
+                            </h1>
+                            <div className="flex flex-col text-gray-700">
+                                <p className="font-medium">
+                                    Written by
+                                    {' '}
+                                    {authors.join(', ')}
+                                </p>
+                                <p className="font-medium">
+                                    Posted:
+                                    {' '}
+                                    {`${date.substring(0, 4)}/${date.substring(5, 7)}/${date.substring(8)}`}
+                                </p>
                             </div>
                         </div>
-                    </ul>
-                </Link>
-            ))}
-        </div>
-    </>
+                    </div>
+                </ul>
+            </Link>
+        ))}
+    </div>
 );
 
 export const getStaticProps: GetStaticProps<BlogProps> = async () => {
