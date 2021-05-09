ac1c53a4312479bfd386733b6301551407ef627e
Nathan Innes <nathaninnes001@gmail.com>
2021-05-09 14:52:15 +0100

    refactor: cleanup and ui tweaks on index

--- pages/notams/index.tsx
+++ pages/notams/index.tsx
@@ -8,80 +8,79 @@ export type BlogProps = { listings: PostListing[] }
 const Blog: React.FC<BlogProps> = ({ listings }) => (
     <>
         <div className="min-h-screen max-w-4xl lg:max-w-6xl mx-auto mb-20 px-page pt-28 lg:pt-20">
-            <ul className="mt-20 text-lg cursor-pointer">
-                {listings.map(({
-                    id,
-                    date,
-                    title,
-                    authors,
-                    readingStats,
-                    category,
-                    metaImage,
-                    metaAlt,
-                }) => (
-                    <Link href={`/notams/${id}`}>
-                        <li key={id}>
-                            <div className="
+            {listings.map(({
+                id,
+                date,
+                title,
+                authors,
+                readingStats,
+                category,
+                metaImage,
+                metaAlt,
+            }) => (
+                <Link href={`/notams/${id}`}>
+                    <ul key={id}>
+                        <div className="
                                 w-full
-                                flex flex-col
                                 box-content
-                                bg-white
-                                mb-5
+                                my-12
+                                flex flex-col
+                                bg-gray-100
                                 rounded-lg
-                                mt-10
-                                transform
-                                hover:scale-105
-                                duration-300
                                 shadow-xl
-                                overflow-hidden"
-                            >
-                                {metaImage
-                                    ? (
-                                        <img
-                                            className="mb-4 max-h-80 object-cover"
-                                            src={metaImage}
-                                            alt={metaAlt}
-                                        />
-                                    )
-                                    : <></>}
-                                <div className="px-7 py-4">
-                                    <div className="xs:flex flex-row gap-x-4">
-                                        {category === 'ANNOUNCEMENTS'
-                                            ? (
-                                                <p
-                                                    className="text-teal-light font-black"
-                                                >
-                                                    {category}
-                                                </p>
-                                            )
-                                            : (
-                                                <p
-                                                    className="text-orangutanOrange font-black"
-                                                >
-                                                    {category}
-                                                </p>
-                                            )}
-                                        <span className="text-black font-medium">
-                                            {readingStats.text}
-                                        </span>
-                                        <p className="text-black font-medium ml-auto">
-                                            {`${date.substring(5, 7)}/${date.substring(8)}/${date.substring(0, 4)}`}
-                                        </p>
-                                    </div>
-                                    <h1 className="text-5xl text-black font-bold leading-tight">
-                                        {title}
-                                    </h1>
-                                    <p className="text-xl text-black font-medium mb-3.5">
-                                        by
+                                overflow-hidden
+                                cursor-pointer"
+                        >
+                            {metaImage
+                                ? (
+                                    <img
+                                        className="mb-2 max-h-80 object-cover"
+                                        src={metaImage}
+                                        alt={metaAlt}
+                                    />
+                                )
+                                : <></>}
+                            <div className="px-7 py-4">
+                                <div className="text-lg flex">
+                                    {category === 'ANNOUNCEMENTS'
+                                        ? (
+                                            <p
+                                                className="text-teal-light font-black"
+                                            >
+                                                {category}
+                                            </p>
+                                        )
+                                        : (
+                                            <p
+                                                className="text-orangutanOrange font-black"
+                                            >
+                                                {category}
+                                            </p>
+                                        )}
+                                    <span className="text-gray-700 font-medium pl-2">
+                                        {readingStats.text}
+                                    </span>
+                                </div>
+                                <h1 className="text-5xl text-black font-bold leading-tight">
+                                    {title}
+                                </h1>
+                                <div className="flex flex-col text-gray-700">
+                                    <p className="font-medium">
+                                        Written by
                                         {' '}
                                         {authors.join(', ')}
                                     </p>
+                                    <p className="font-medium">
+                                        Posted:
+                                        {' '}
+                                        {`${date.substring(0, 4)}/${date.substring(5, 7)}/${date.substring(8)}`}
+                                    </p>
                                 </div>
                             </div>
-                        </li>
-                    </Link>
-                ))}
-            </ul>
+                        </div>
+                    </ul>
+                </Link>
+            ))}
         </div>
     </>
 );
