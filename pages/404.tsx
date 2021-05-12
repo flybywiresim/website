479fb766017137d9b372060d74dba413069b7708
Nathan Innes <nathaninnes001@gmail.com>
2021-05-12 12:47:17 +0100

    refactor: cleanup 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 
 const Custom404: React.FC = () => (
-    <>
-        <div className="relative h-screen mx-auto px-2 pt-80 max-w-md md:max-w-4xl text-center z-20">
-            <div className="h-full">
-                <h5 className="text-9xl text-blue-light font-bold">404</h5>
-                <h2 className="mt-8 text-4xl font-bold">
-                    We don&apos;t know where the baggage is! It seems like the URL that you requested from us doesn&apos;t exist.
-                </h2>
-                <h1 className="mt-4 text-xl text-grey-light font-medium">
-                    If you believe this was a mistake, reach out to us through our Discord,
-                    or return to the homepage.
-                </h1>
-            </div>
+    <section>
+        <div className="flex flex-col min-h-screen mx-auto justify-center max-w-md md:max-w-4xl text-center">
+            <h5 className="text-9xl text-blue-light font-bold">404</h5>
+            <h2 className="mt-8 text-4xl font-bold">
+                We don&apos;t know where the baggage is! It seems like the URL that you requested from us doesn&apos;t exist.
+            </h2>
+            <h1 className="mt-4 text-xl text-grey-light font-medium">
+                If you believe this was a mistake, reach out to us through our Discord,
+                or return to the homepage.
+            </h1>
         </div>
+
         <video
             className="absolute top-0 h-screen object-cover w-screen opacity-10"
             src="video/A32NX.mp4"
@@ -22,7 +21,7 @@ const Custom404: React.FC = () => (
             muted
             loop
         />
-    </>
+    </section>
 );
 
 export default Custom404;
