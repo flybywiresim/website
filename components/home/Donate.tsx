a0d89aeca0bb8b4179268899e96d39dccd89ca91
Nathan Innes <nathaninnes001@gmail.com>
2021-05-14 17:53:00 +0100

    style: fix donate button colour

--- components/home/Donate.tsx
+++ components/home/Donate.tsx
@@ -10,8 +10,8 @@ export const Donate: React.FC = () => (
                     <h1 className="text-5xl text-teal font-semibold">Donate</h1>
 
                     <p className="xl:w-2/3 text-lg sm:text-xl mt-6">
-                        {/* eslint-disable-next-line max-len */}
-                        Contributions allow FlyByWire Simulations to provide better services for our users, such as fast and reliable download networks and integrated online features to enrich your flight simulation experience.
+                        Contributions allow FlyByWire Simulations to provide better services for our users,
+                        such as fast and reliable download networks and integrated online features to enrich your flight simulation experience.
                     </p>
                     <p className="xl:w-2/3 text-lg sm:text-xl mt-6">
                         We use Open Collective to transparently manage our donations and expenses, which means you can see how we utilize donated funds at any time.
@@ -20,7 +20,9 @@ export const Donate: React.FC = () => (
 
                 <div className="mt-6 xl:divide-x divide-gray-400 xl:space-x-7 flex flex-col-reverse xl:flex-row items-center">
                     <a href="https://opencollective.com/flybywire/" target="_blank" rel="noreferrer">
-                        <Button className="w-40 text-xl font-bold bg-teal border-2 border-blue-light transition hover:bg-white hover:text-blue-light">Donate</Button>
+                        <Button className="w-40 text-xl font-bold bg-teal-light-contrast border-2 border-teal-light-contrast transition hover:bg-white hover:text-teal-light-contrast">
+                            Donate
+                        </Button>
                     </a>
                     <h2 className="max-w-prose text-teal-50 h-14 mb-7 xl:mb-0 xl:pl-7 flex flex-row items-center">
                         No profit is generated from donations. Donating does not guarantee access to additional content nor its exclusivity if such content is ever given.
