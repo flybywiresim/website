fc3283cfdc7a90daca99f9dc33ebc1a5f37c6f8b
Erick Torres <30361843+ErickSharp@users.noreply.github.com>
2021-05-23 09:57:15 -0500

    refactor: make navbar close when clicking on link and click outside of menu (#223)

--- src/components/utils/NavBar.tsx
+++ src/components/utils/NavBar.tsx
@@ -17,21 +17,26 @@ export function NavBar(): JSX.Element {
     const [isOpen, setOpen] = useState(false);
 
     return (
-        <nav className={`fixed top-0 left-0 right-0 mx-auto py-2 z-50 ${scroll || isOpen ? 'transition bg-blue-dark-contrast shadow-lg-dark' : 'transition bg-transparent'}`}>
-            <div className="max-w-6xl mx-auto px-page">
-                <div className="relative flex items-center justify-between h-16">
-                    <Link href="/">
-                        <Image className="cursor-pointer" src="/svg/white/FBW-Logo-WHITE.svg" width={180} height={40} />
-                    </Link>
-                    <div className="absolute inset-y-0 right-3 flex items-center md:hidden">
-                        <Hamburger handleClick={() => setOpen((prevState) => !prevState)} />
-                    </div>
-                    <div className="flex flex-row items-center">
-                        <NavLinks className="hidden md:flex md:flex-row" />
+        <nav className="flex flex-col fixed top-0 left-0 right-0 mx-auto z-50">
+            <div className={`py-0 md:py-2 ${scroll || isOpen ? 'transition bg-blue-dark-contrast shadow-lg-dark' : 'transition bg-transparent'}`}>
+                <div className="max-w-6xl mx-auto px-page">
+                    <div className="relative flex items-center justify-between h-16">
+                        <Link href="/">
+                            <Image className="cursor-pointer" src="/svg/white/FBW-Logo-WHITE.svg" width={180} height={40} />
+                        </Link>
+                        <div className="absolute inset-y-0 right-3 flex items-center md:hidden">
+                            <Hamburger handleClick={() => setOpen((prevState) => !prevState)} />
+                        </div>
+                        <div className="flex flex-row items-center">
+                            <NavLinks className="hidden md:flex md:flex-row" />
+                        </div>
                     </div>
                 </div>
+                <div onClick={() => setOpen(!isOpen)}>
+                    <NavLinks className={`px-page md:px-0 ${isOpen ? 'md:hidden' : 'hidden'}`} />
+                </div>
             </div>
-            <NavLinks className={`px-page md:px-0 ${isOpen ? 'md:hidden' : 'hidden'}`} />
+            <div className={`h-screen ${isOpen ? 'md:hidden' : 'hidden'}`} onClick={() => setOpen(!isOpen)} />
         </nav>
     );
 }
