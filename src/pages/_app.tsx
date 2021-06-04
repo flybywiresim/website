1e34bcd78ec16cf9389310bccc044262ab47540d
Benjamin Dupont <benjozorkfr@gmail.com>
2021-06-04 02:07:31 -0400

    feat: add embed previews to NOTAMs (#222)
    fix: change theme color
    
    fix: increase NOTAMs embed preview font size
    
    fix: make sure site-wide embeds are preserved
    
    feat: add image to NOTAM embed preview
    
    fix: bad og tag overrides
    
    fix: use UUID for preview file names
    
    fix: change NOTAM embed description
    
    fix: package-lock.json

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,8 +8,33 @@ import '../styles/main.scss';
 const App = ({ Component, pageProps }: AppProps) => (
     <div className="root">
         <Head>
+            <meta charSet="utf-8" />
+
+            <meta name="theme-color" content="#00c2cc" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+
             <title>FlyByWire Simulations</title>
+
+            <meta
+                key="description"
+                name="description"
+                content="Official FlyByWire Simulations website, an open-source community providing high quality addons for Microsoft Flight Simulator."
+            />
+            <meta
+                key="og:title"
+                name="og:title"
+                content="FlyByWire Simulations"
+            />
+            <meta
+                key="og:description"
+                name="og:description"
+                content="Official FlyByWire Simulations website, an open-source community providing high quality addons for Microsoft Flight Simulator."
+            />
+            <meta
+                key="og:image"
+                name="og:image"
+                content="/A32NX_Front.jpeg"
+            />
         </Head>
         <NavBar />
         <ScrollToTopButton />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,26 +9,7 @@ class AppDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                <Head>
-                    <meta charSet="utf-8" />
-                    <meta name="theme-color" content="#1D2838" />
-                    <meta
-                        name="description"
-                        content="Official FlyByWire Simulations website, an open-source community providing high quality addons for Microsoft Flight Simulator."
-                    />
-                    <meta
-                        name="og:title"
-                        content="FlyByWire Simulations"
-                    />
-                    <meta
-                        name="og:description"
-                        content="Official FlyByWire Simulations website, an open-source community providing high quality addons for Microsoft Flight Simulator."
-                    />
-                    <meta
-                        name="og:image"
-                        content="/A32NX_Front.jpeg"
-                    />
-                </Head>
+                <Head />
                 <body>
                     <Main />
                     <NextScript />
