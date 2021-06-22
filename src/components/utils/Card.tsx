90bb04ebd65b8eeb6cf832ebcffea9de4536ed91
Nathan Innes <nathaninnes001@gmail.com>
2021-06-23 00:45:14 +0100

    fix(card): margin value and component cleanup

--- src/components/utils/Card.tsx
+++ src/components/utils/Card.tsx
@@ -1,21 +1,22 @@
-import { FC } from 'react';
+import { ReactNode } from 'react';
 
-type cardProps = {
+type CardProps = {
     className?: string,
     bgColour: string,
+    children: ReactNode
 }
-export const Card: FC<cardProps> = (props) => (
+export const Card = (props: CardProps) => (
     <div className={`bg-${props.bgColour} border-t-4 px-page py-6 max-w-2xl rounded-lg ${props.className}`}>
         {props.children}
     </div>
 );
 
-export const CardTitle: FC = ({ children }) => (
+export const CardTitle = ({ children }: CardProps) => (
     <h2 className="flex items-center">{children}</h2>
 );
 
-export const CardBody: FC = ({ children }) => (
-    <p className="mb-4">
+export const CardBody = ({ children }: CardProps) => (
+    <p className="mb-8">
         {children}
     </p>
 );
