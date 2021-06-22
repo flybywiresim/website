92eeb193f213e1eec9eac881c42c1037dfe0fc3c
Nathan Innes <nathaninnes001@gmail.com>
2021-06-22 10:52:59 +0100

    refactor(a320header): cleanup component

--- src/components/home/headers/A320Header.tsx
+++ src/components/home/headers/A320Header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { Button } from '../../utils/Button';
 import { installerRef } from '../Installer';
+import Container from '../../utils/Container';
 
 export const A320Header = () => {
     const handleClick = () => {
@@ -10,20 +11,27 @@ export const A320Header = () => {
     };
 
     return (
-        <>
-            <header className="h-screen">
-                <div className="absolute max-w-6xl mx-auto px-page inset-x-2 inset-y-1/3 z-30 ">
+        <header className="min-h-screen">
+            <video
+                className="fixed w-screen h-screen object-cover opacity-10"
+                src="https://cdn.flybywiresim.com/assets/website/video/A32NX.mp4"
+                playsInline
+                autoPlay
+                muted
+                loop
+            />
+            <Container className="max-w-6xl">
+                <div className="absolute h-2/3 bottom-0">
                     <h1 className="text-7xl sm:text-8xl font-medium pb-3">
                         <span className="text-blue-light">A32</span>
                         <span className="text-blue">N</span>
                         X
                     </h1>
-                    <p className="text-lg max-w-lg">
+                    <p className="text-lg max-w-xl">
                         A community built and maintained project aiming to provide high quality and detailed aircraft
                         for Microsoft Flight Simulator.
                     </p>
                     <div className="flex max-w-md py-5">
-
                         <Button
                             onClick={handleClick}
                             className="w-40 text-xl font-bold bg-blue-light border-2 border-blue-light transition hover:bg-white hover:text-blue-light mr-3"
@@ -40,16 +48,7 @@ export const A320Header = () => {
                         </Link>
                     </div>
                 </div>
-
-                <video
-                    className="fixed w-screen h-screen object-cover opacity-10 -z-10"
-                    src="https://cdn.flybywiresim.com/assets/website/video/A32NX.mp4"
-                    playsInline
-                    autoPlay
-                    muted
-                    loop
-                />
-            </header>
-        </>
+            </Container>
+        </header>
     );
 };
