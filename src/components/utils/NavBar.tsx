351dc417fb02edd8d02798c8277b18bfcebf27c4
ErickSharp <titaninmc@gmail.com>
2021-06-26 03:22:21 -0500

    smaller bars

--- src/components/utils/NavBar.tsx
+++ src/components/utils/NavBar.tsx
@@ -33,9 +33,9 @@ export function NavBar(): JSX.Element {
                         <Image className="cursor-pointer" src="/svg/white/FBW-Logo-WHITE.svg" width={180} height={40} />
                     </Link>
                     <span className="flex flex-col place-content-center w-7 h-10 transition cursor-pointer md:hidden" onClick={() => setOpen(!isOpen)}>
-                        <div className={`${isOpen && 'rotate-45 translate-y-1'} duration-200 h-1 bg-white rounded-full`} />
-                        <div className={`${isOpen ? 'h-0' : 'h-1 my-1'} bg-white rounded-full`} />
-                        <div className={`${isOpen && '-rotate-45'}  duration-200 h-1 bg-white rounded-full`} />
+                        <div className={`${isOpen && 'rotate-45 translate-y-[3px]'} duration-200 h-[3px] bg-white rounded-full`} />
+                        <div className={`${isOpen ? 'h-0' : 'h-[3px] my-[5px]'} bg-white rounded-full`} />
+                        <div className={`${isOpen && '-rotate-45'}  duration-200 h-[3px] bg-white rounded-full`} />
                     </span>
                     <NavLinks className="hidden md:flex md:flex-row" />
                 </div>
