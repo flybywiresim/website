6a9ab3101564da5fca32c50f0a1d5267c5988fbe
Erick Torres <30361843+ErickSharp@users.noreply.github.com>
2021-06-26 19:58:20 -0500

    feat: make home navbar link not show on home page (#247)

--- src/components/utils/NavLinks.tsx
+++ src/components/utils/NavLinks.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { Dropdown, DropdownItem } from './Dropdown';
 
 export type LinkProps = {
@@ -22,29 +23,33 @@ export const NavLink = ({ name, path, external, className }: LinkProps) => (
     </li>
 );
 
-export const NavLinks = (props: { className?: string }) => (
-    <span className={`flex flex-col gap-x-4 gap-y-2 md:items-center ${props.className}`}>
-        <NavLink name="Home" path="/" />
-        <NavLink name="NOTAMS" path="/notams" />
-        <NavLink name="Projects" path="/a32nx" />
-        <NavLink name="Documentation" external path="https://docs.flybywiresim.com/" />
-        <NavLink name="Map" path="/map" />
-        <Dropdown titleName="Community">
-            <DropdownItem>
-                <NavLink name="Discord" path="https://discord.gg/flybywire" />
-            </DropdownItem>
-            <DropdownItem>
-                <NavLink name="Twitter" external path="https://twitter.com/FlyByWireSim" />
-            </DropdownItem>
-            <DropdownItem>
-                <NavLink name="Facebook" external path="https://facebook.com/FlybywireSimulations/" />
-            </DropdownItem>
-            <DropdownItem>
-                <NavLink name="YouTube" external path="https://www.youtube.com/FlyByWireSimulations" />
-            </DropdownItem>
-            <DropdownItem>
-                <NavLink name="Donate" external path="https://opencollective.com/flybywire/" />
-            </DropdownItem>
-        </Dropdown>
-    </span>
-);
+export const NavLinks = (props: { className?: string }) => {
+    const router = useRouter();
+
+    return (
+        <span className={`flex flex-col gap-x-4 gap-y-2 md:items-center ${props.className}`}>
+            {router.pathname !== '/' && <NavLink name="Home" path="/" />}
+            <NavLink name="NOTAMS" path="/notams" />
+            <NavLink name="Projects" path="/a32nx" />
+            <NavLink name="Documentation" external path="https://docs.flybywiresim.com/" />
+            <NavLink name="Map" path="/map" />
+            <Dropdown titleName="Community">
+                <DropdownItem>
+                    <NavLink name="Discord" path="https://discord.gg/flybywire" />
+                </DropdownItem>
+                <DropdownItem>
+                    <NavLink name="Twitter" external path="https://twitter.com/FlyByWireSim" />
+                </DropdownItem>
+                <DropdownItem>
+                    <NavLink name="Facebook" external path="https://facebook.com/FlybywireSimulations/" />
+                </DropdownItem>
+                <DropdownItem>
+                    <NavLink name="YouTube" external path="https://www.youtube.com/FlyByWireSimulations" />
+                </DropdownItem>
+                <DropdownItem>
+                    <NavLink name="Donate" external path="https://opencollective.com/flybywire/" />
+                </DropdownItem>
+            </Dropdown>
+        </span>
+    );
+};
