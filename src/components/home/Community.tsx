c546b5d1d0447867f17fadee0dad5526da1dc674
Frank Kopp <frank@familie-kopp.de>
2022-01-26 02:20:58 +0100

    feat: Add id to be able to anchor link the homepage's community section (#359)
    * Add id="community" to be able to anchor link the homepage's community section
    
    * Fix lint

--- src/components/home/Community.tsx
+++ src/components/home/Community.tsx
@@ -49,7 +49,7 @@ export const Community = () => {
         return () => clearInterval(interval);
     }, []);
     return (
-        <section className="flex flex-col justify-between items-center text-blue-dark-contrast bg-gray-50 lg:flex-row">
+        <section id="community" className="flex flex-col justify-between items-center text-blue-dark-contrast bg-gray-50 lg:flex-row">
             <Container className="flex flex-col py-12 max-w-6xl lg:px-24">
                 <span className="mb-3 w-24 h-2 bg-teal-light-contrast rounded-full" />
                 <h1>Community Insights</h1>
