3943664055d120dcec590404a0c5cc7ddeb1dac3
Louis Devota <hyperman738@gmail.com>
2022-06-11 20:17:25 -0700

    rework donation

--- src/components/home/Donate.tsx
+++ src/components/home/Donate.tsx
@@ -1,23 +1,25 @@
 import { Button } from '../utils/Button';
-import Container from '../utils/Container';
 
 export const Donate = () => (
-    <section className="bg-blue-darker">
-        <div className="relative">
-            <img className="object-cover absolute w-full h-full opacity-10" src="/img/donate.jpg" alt="" />
+    <section className="h-[495px]">
+        <div
+            className="relative w-full h-full bg-navy-dark bg-[center_-12.8rem]"
+            style={{ backgroundImage: 'url("/svg/airfoil.svg")' }}
+        >
+            {/* <Image src="/svg/airfoil.svg" width={1920} height={705.02} className="object-cover absolute opacity-[3%]"/> */}
 
-            <Container className="flex relative top-0 flex-col items-center py-12 max-w-7xl lg:items-start">
+            <div className="flex flex-col justify-center py-12 h-full lg:items-start px-page">
                 <div className="max-w-4xl text-center lg:text-left">
-                    <h1 className="text-teal">Donate</h1>
-                    <p>
+                    <h1 className="text-cyan">Donate</h1>
+                    <p className="mt-6">
                         Contributions allow FlyByWire Simulations to provide better services for our users,
                         such as fast and reliable download networks and integrated online features to enrich your flight simulation experience.
                     </p>
-                    <p className="py-4">
+                    <p className="mt-6">
                         We use Open Collective to transparently manage our donations and expenses, which means you can see how we utilize donated funds at any time.
                     </p>
                 </div>
-                <div className="flex flex-col items-center space-y-3 space-x-4 text-center md:space-y-0 lg:flex-row lg:items-start lg:text-left">
+                <div className="flex flex-col items-center mt-6 space-y-3 space-x-4 text-center md:space-y-0 lg:flex-row lg:items-start lg:text-left">
                     <a href="https://opencollective.com/flybywire/" target="_blank" rel="noreferrer">
                         <Button className="w-40 hover:text-teal-light-contrast bg-teal-light-contrast hover:bg-white border-2 border-teal-light-contrast">
                             Donate
@@ -27,7 +29,7 @@ export const Donate = () => (
                         *No profit is generated from donations. Donating does not guarantee access to additional content nor its exclusivity if such content is ever given.
                     </em>
                 </div>
-            </Container>
+            </div>
         </div>
     </section>
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { A320Header } from '../components/home/A320Header';
 import { Installer } from '../components/home/Installer';
 import { Community } from '../components/home/Community';
-import { PartnerSection } from '../components/home/PartnerSection';
 import { Donate } from '../components/home/Donate';
 
 const Index = () => (
@@ -9,7 +8,6 @@ const Index = () => (
         <A320Header />
         <Installer />
         <Community />
-        <PartnerSection />
         <Donate />
     </>
 );
