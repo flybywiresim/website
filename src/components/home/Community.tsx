e4c50339d0f3dcf0ad1c6c5baf77721d8692583b
Louis Devota <hyperman738@gmail.com>
2022-06-12 00:05:11 -0700

    general improvements

--- src/components/home/Community.tsx
+++ src/components/home/Community.tsx
@@ -17,8 +17,8 @@ const CONTRIBUTOR_COUNT_ENDPOINT = 'https://api.github.com/repos/flybywiresim/a3
 
 const Statistic = (props: {statCount: string, statName: string}) => (
     <div id={props.statName}>
-        <h1 className="text-5xl font-bold text-blue-dark-contrast">{props.statCount}</h1>
-        <p>{props.statName}</p>
+        <h1 className="text-5xl font-semiold">{props.statCount}</h1>
+        <p className="text-base opacity-75">{props.statName}</p>
     </div>
 );
 
@@ -49,49 +49,44 @@ export const Community = () => {
         return () => clearInterval(interval);
     }, []);
     return (
-        <section id="community" className="flex flex-col justify-between items-center text-blue-dark-contrast bg-gray-50 lg:flex-row">
-            <Container className="flex flex-col py-12 max-w-6xl lg:px-24">
-                <span className="mb-3 w-24 h-2 bg-teal-light-contrast rounded-full" />
+        <section id="community" className="flex flex-col justify-between text-black bg-gray-50 lg:flex-row lg:items-center">
+            <div className="relative z-10 w-full h-144 lg:w-1/2 lg:h-200">
+                <MapDisplay refreshInterval={mapRefreshInterval} disableMenu disableWeather={false} disableScroll forceTileset="carto-light" />
+            </div>
+            <Container className="flex flex-col py-7 max-w-6xl">
+
                 <h1>Community Insights</h1>
-                <p className="max-w-prose text-gray-800">
+                <p className="mt-5 max-w-prose opacity-75">
                     Discover the extensive community behind every FlyByWire Simulations aircraft - a vibrant and active online group that prioritises collaborative work and openness.
                 </p>
 
-                <div className="grid grid-cols-2 gap-y-8 my-6 sm:grid-cols-4">
+                <div className="flex flex-wrap gap-x-16 gap-y-6 my-6 sm:grid-cols-4">
                     <Statistic statCount={liveFlights} statName="Live Flights" />
                     <Statistic statCount={commitCount} statName="Commits" />
                     <Statistic statCount={contributorCount} statName="Contributors" />
                     <Statistic statCount="1M+" statName="Downloads" />
                 </div>
 
-                <div className="my-8">
-                    <Card bgColour="gray-100" className="border-discord shadow-lg">
-                        <CardTitle>
-                            <span className="flex justify-center items-center p-2 mr-2 bg-discord rounded-xl">
-                                <Image src="/svg/discordLogo.svg" width={23} height={23} />
-                            </span>
-                            Discord
-                        </CardTitle>
-                        <CardBody>
-                            Our Discord server is where we plan the entirety of our projects and provide most of our support.
-                            Join us to chat with other members of the community, get started with contributing, or ask us a question!
-                        </CardBody>
+                <hr className="my-6" />
 
-                        <a
-                            className="self-center py-2 px-3 text-xl font-semibold text-white bg-discord hover:bg-discord-dark rounded-xl transition"
-                            href="https://discord.gg/flybywire"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            Join the Community
-                            <IconArrowRight className="inline-flex mb-1 ml-1" size={20} stroke={2} />
-                        </a>
-                    </Card>
+                <div className="flex space-x-5">
+                    <Image src="/svg/Discord-Icon.svg" width={45} height={34.86} />
+                    <h1 className="mt-2 text-3xl text-discord">Discord</h1>
                 </div>
+                <p className="pt-5 max-w-prose opacity-75">
+                    Join us to chat with other members of the community, get started with contributing, or ask us a question!
+                </p>
+                <button type="button" className="self-start py-2 px-6 mt-5 font-semibold text-white bg-discord hover:bg-discord-dark rounded-lg text-md">
+                    <a
+                        href="https://discord.gg/flybywire"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Join the Community
+                    </a>
+                </button>
+
             </Container>
-            <div className="relative z-10 w-full h-144 lg:w-1/2 lg:h-200">
-                <MapDisplay refreshInterval={mapRefreshInterval} disableMenu disableWeather={false} disableScroll forceTileset="carto-light" />
-            </div>
         </section>
     );
 };
--- src/components/home/Donate.tsx
+++ src/components/home/Donate.tsx
@@ -1,4 +1,4 @@
-import { Button } from '../utils/Button';
+import { Button, ButtonType } from '../utils/Button';
 
 export const Donate = () => (
     <section className="h-[495px]">
@@ -9,7 +9,7 @@ export const Donate = () => (
             {/* <Image src="/svg/airfoil.svg" width={1920} height={705.02} className="object-cover absolute opacity-[3%]"/> */}
 
             <div className="flex flex-col justify-center py-12 h-full lg:items-start px-page">
-                <div className="max-w-4xl text-center lg:text-left">
+                <div className="max-w-4xl">
                     <h1 className="text-cyan">Donate</h1>
                     <p className="mt-6">
                         Contributions allow FlyByWire Simulations to provide better services for our users,
@@ -19,13 +19,13 @@ export const Donate = () => (
                         We use Open Collective to transparently manage our donations and expenses, which means you can see how we utilize donated funds at any time.
                     </p>
                 </div>
-                <div className="flex flex-col items-center mt-6 space-y-3 space-x-4 text-center md:space-y-0 lg:flex-row lg:items-start lg:text-left">
+                <div className="flex flex-col gap-x-4 gap-y-3 mt-6 lg:flex-row">
                     <a href="https://opencollective.com/flybywire/" target="_blank" rel="noreferrer">
-                        <Button className="w-40 hover:text-teal-light-contrast bg-teal-light-contrast hover:bg-white border-2 border-teal-light-contrast">
+                        <Button type={ButtonType.Neutral} className="w-40">
                             Donate
                         </Button>
                     </a>
-                    <em className="self-center max-w-prose text-teal-100">
+                    <em className="max-w-prose text-teal-100">
                         *No profit is generated from donations. Donating does not guarantee access to additional content nor its exclusivity if such content is ever given.
                     </em>
                 </div>
