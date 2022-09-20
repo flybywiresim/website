5c8885061afd59cd9f1876f15cff1691e3c727ca
Nathan Innes <nathaninnes001@gmail.com>
2022-09-21 00:27:24 +0100

    refactor: streamline bgVideo component into layout

--- src/layouts/landingPage.tsx
+++ src/layouts/landingPage.tsx
@@ -2,7 +2,24 @@ import { ReactNode } from 'react';
 import Section from '../components/Utils/Section';
 import Container from '../components/Utils/Container';
 import Button from '../components/Button/Button';
-import BackgroundVideo from '../components/Utils/BackgroundVideo';
+
+const BackgroundVideo = () => {
+    // TODO: Decide if we want to use image or video?
+    const videoLink = 'https://cdn.discordapp.com/attachments/740722295009706034/1021579447649976390/Microsoft_Flight_Simulator_2022.09.19_-_17.05.19.07.DVR_Trim.mp4';
+
+    return (
+        <video
+            autoPlay
+            muted
+            loop
+            controls={false}
+            draggable={false}
+            playsInline
+            src={videoLink}
+            className="absolute top-0 left-0 -z-50 h-full w-screen object-cover"
+        />
+    );
+};
 
 const ButtonGroup = (props: {children: ReactNode}) => (
     <ul className="my-4 grid grid-cols-2 gap-4">
