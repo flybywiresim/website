70e12c089d933c330372a5a27971f5ab686c2169
Nathan Innes <nathaninnes001@gmail.com>
2022-09-22 15:10:57 +0100

    refactor: bg image placeholder

--- src/layouts/landingPage.tsx
+++ src/layouts/landingPage.tsx
@@ -1,25 +1,14 @@
 import { ReactNode } from 'react';
+import Image from 'next/image';
 import Section from '../components/Utils/Section';
 import Container from '../components/Utils/Container';
 import Button from '../components/Button/Button';
 
-const BackgroundVideo = () => {
-    // TODO: Decide if we want to use image or video?
-    const videoLink = 'https://cdn.discordapp.com/attachments/740722295009706034/1021579447649976390/Microsoft_Flight_Simulator_2022.09.19_-_17.05.19.07.DVR_Trim.mp4';
-
-    return (
-        <video
-            autoPlay
-            muted
-            loop
-            controls={false}
-            draggable={false}
-            playsInline
-            src={videoLink}
-            className="absolute top-0 left-0 -z-50 h-full w-screen object-cover"
-        />
-    );
-};
+const BackgroundImage = () => (
+    <div className="absolute -z-10 h-screen w-screen opacity-90">
+        <Image src="/img/bg.png" layout="fill" objectFit="cover" />
+    </div>
+);
 
 const ButtonGroup = (props: {children: ReactNode}) => (
     <ul className="my-4 grid grid-cols-2 gap-4">
@@ -29,8 +18,8 @@ const ButtonGroup = (props: {children: ReactNode}) => (
 
 const LandingPage = () => (
     <>
-        <Section theme="dark" className="relative bg-dark/95 bg-gradient-to-bl from-primary/10 to-transparent">
-            <BackgroundVideo />
+        <Section theme="dark" className="relative bg-dark/75 bg-gradient-to-bl from-primary/30 to-transparent">
+            <BackgroundImage />
             <Container className="h-screen" center>
                 <h4 className="font-normal leading-4 text-primary">FlyByWire Simulations</h4>
                 <h1>A32NX</h1>
