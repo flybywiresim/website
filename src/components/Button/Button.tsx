a7964361fefa4ef41bfc8ec9ed0000c7e3d97ae5
Nathan Innes <nathaninnes001@gmail.com>
2022-09-23 04:35:20 +0100

    style: update button primary color

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,7 +9,7 @@ const Button = ({ label, theme, className }: ButtonProps) => (
     <button
         type="button"
         className={`
-        ${theme === 'primary' ? 'bg-primary text-secondary' : 'bg-secondary-accent text-light'} 
+        ${theme === 'primary' ? 'bg-primary-accent text-secondary' : 'bg-secondary-accent text-light'} 
         w-full rounded-md px-8 py-2 font-body text-lg font-semibold
         ${className}
     `}
