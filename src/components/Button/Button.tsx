43d9896bb52fdc2d8dfb3d55bb921acf093a2f32
Nathan Innes <nathaninnes001@gmail.com>
2022-09-23 03:31:36 +0100

    feat: className on button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,19 @@
 type ButtonProps = {
     label: string
-    theme: 'primary' | 'secondary';
+    theme?: 'primary' | 'secondary';
+    className?: string;
     onClick?: () => void
 }
 // TODO: Add onClick handler
-const Button = ({ label, theme }: ButtonProps) => (
-    <button type="button" className={`${theme === 'primary' ? 'bg-primary text-secondary' : 'bg-secondary-accent text-light'} w-full rounded-md px-8 py-2 font-body text-lg font-semibold`}>
+const Button = ({ label, theme, className }: ButtonProps) => (
+    <button
+        type="button"
+        className={`
+        ${theme === 'primary' ? 'bg-primary text-secondary' : 'bg-secondary-accent text-light'} 
+        w-full rounded-md px-8 py-2 font-body text-lg font-semibold
+        ${className}
+    `}
+    >
         {label}
     </button>
 );
