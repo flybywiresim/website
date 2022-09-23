a1579493eb560e4666066568b707cd53bd04f292
Nathan Innes <nathaninnes001@gmail.com>
2022-09-23 03:33:12 +0100

    refactor: rename installer to uppercase and general style changes

--- src/layouts/landing/Installer.tsx
+++ src/layouts/landing/Installer.tsx
@@ -3,8 +3,8 @@ import Section from '../../components/Utils/Section';
 import Container from '../../components/Utils/Container';
 import Button from '../../components/Button/Button';
 
-const installer = () => (
-    <Section theme="dark" className="grid gap-y-20 bg-gradient-to-r from-transparent to-secondary-accent/50 py-20">
+const Installer = () => (
+    <Section theme="dark" className="grid gap-y-20">
         <Container className="grid gap-y-5">
             <h2>Custom-built Installer</h2>
             <p>
@@ -14,10 +14,10 @@ const installer = () => (
         </Container>
 
         {/* TODO: Change placeholder image */}
-        <div className="-mb-32">
-            <Image src="/img/installerPreview.png" layout="intrinsic" width={1920} height={1080} />
+        <div className="z-10 -mb-32">
+            <Image src="/img/installerPreview.png" layout="responsive" width={1920} height={1080} />
         </div>
     </Section>
 );
 
-export default installer;
+export default Installer;
