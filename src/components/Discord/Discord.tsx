ff9d0f0bd2fafcbcfc693627a897aacc2dae6466
Nathan Innes <nathaninnes001@gmail.com>
2022-09-23 03:34:46 +0100

    feat: discord component

---
+++ src/components/Discord/Discord.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Button from '../Button/Button';
+
+const Discord = () => (
+    <div className="grid gap-y-5">
+        <span className="flex justify-center gap-x-5">
+            <Image src="/svg/discord.svg" layout="intrinsic" width={40} height={40} />
+            <h3 className="text-discord">Discord</h3>
+        </span>
+        <p>Join us to chat with other members of the community, get started with contributing, or ask us a question!</p>
+
+        <a
+            href="https://discord.gg/flybywire"
+            target="_blank"
+            rel="noreferrer"
+        >
+            <Button label="Join our Community!" className="bg-discord" />
+        </a>
+    </div>
+);
+
+export default Discord;
