f638b18dad6251de326a2ea2db17cadcf8aa0f4e
Nathan Innes <nathaninnes001@gmail.com>
2022-09-26 05:31:28 +0100

    style: adjust terms position

--- src/components/Navigation/Footer.tsx
+++ src/components/Navigation/Footer.tsx
@@ -22,25 +22,15 @@ const NavItem = (props: {label: string, href: string}) => (
 
 const Footer = () => (
     <footer>
-        <Section>
+        <Section className="py-14">
             <Container className="gap-y-8" display="grid">
                 <Image src="/svg/tail/tail.svg" alt="FlyByWire Simulations Logo" width={40} height={40} />
 
                 <div className="text-dark">
                     <h4>Contact Us</h4>
                     <p className="font-semibold">contact@flybywiresim.com</p>
-                    <div className="text-md grid gap-y-2 py-4">
-                        <span className="grid grid-cols-3">
-                            <a href="https://github.com/flybywiresim/manuals/blob/master/pdf/FBW%20Terms%20of%20Services.pdf" target="_blank" rel="noreferrer">Terms of Use</a>
-                            <a href="https://github.com/flybywiresim/manuals/blob/master/pdf/FBW%20Privacy%20Policy.pdf" target="_blank" rel="noreferrer">Privacy Policy</a>
-                            <a href="https://github.com/flybywiresim/" target="_blank" rel="noreferrer">Source Code</a>
-                        </span>
-                        <small className="text-dark/50">
-                            &copy; FlyByWire Simulations and its contributors 2020-
-                            {new Date().getFullYear()}
-                        </small>
-
-                        <div className="grid grid-cols-3 items-start gap-y-5 py-8">
+                    <div className="text-md py-4">
+                        <div className="grid grid-cols-3 items-start gap-y-5">
                             <NavWrapper label="Projects">
                                 <NavItem label="Installer" href="/" />
                                 <NavItem label="A32NX" href="/" />
@@ -62,6 +52,20 @@ const Footer = () => (
                                 <NavItem label="YouTube" href="/" />
                                 <NavItem label="Twitch" href="/" />
                             </NavWrapper>
+                        </div>
+
+                        <div className="relative">
+                            <div className="absolute top-4 py-8">
+                                <span className="grid grid-cols-3 divide-x text-center">
+                                    <a href="https://github.com/flybywiresim/manuals/blob/master/pdf/FBW%20Terms%20of%20Services.pdf" target="_blank" rel="noreferrer">Terms of Use</a>
+                                    <a href="https://github.com/flybywiresim/manuals/blob/master/pdf/FBW%20Privacy%20Policy.pdf" target="_blank" rel="noreferrer">Privacy Policy</a>
+                                    <a href="https://github.com/flybywiresim/" target="_blank" rel="noreferrer">Source Code</a>
+                                </span>
+                                <small className="text-dark/50">
+                                    &copy; FlyByWire Simulations and its contributors 2020-
+                                    {new Date().getFullYear()}
+                                </small>
+                            </div>
                         </div>
                     </div>
                 </div>
