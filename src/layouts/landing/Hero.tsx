10c85c80cac1450ef6d71f0b7559f8e9d2f5e71f
Nathan Innes <nathaninnes001@gmail.com>
2022-11-03 02:30:07 +0000

    fix: hero image positioning

--- src/layouts/landing/Hero.tsx
+++ src/layouts/landing/Hero.tsx
@@ -5,7 +5,7 @@ import Container from '../../components/Utils/Container';
 import Button from '../../components/Button/Button';
 
 const BackgroundImage = () => (
-    <div className="absolute -z-10 h-screen w-screen opacity-90">
+    <div className="absolute top-0 left-0 -z-10 h-screen w-screen opacity-90">
         <Image src="/pages/index/bg.png" layout="fill" objectFit="cover" />
     </div>
 );
@@ -18,9 +18,9 @@ const ButtonGroup = (props: {children: ReactNode}) => (
 
 const Hero = () => (
     <>
-        <Section className="bg-black/50 bg-gradient-to-bl from-primary/30 to-secondary py-0">
+        <Section className="bg-black/50 bg-gradient-to-bl from-primary/30 to-secondary">
             <BackgroundImage />
-            <Container className="h-screen justify-center" display="flex flex-col">
+            <Container className="h-120 justify-center" display="flex flex-col">
                 <h4 className="font-normal leading-4 text-primary">FlyByWire Simulations</h4>
                 <h1>A32NX</h1>
                 <p className="max-w-xl">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alternation in some form, by injected humor...</p>
