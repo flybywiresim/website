cd9e0064508cfef86949359b1b74a6e1af84d4cd
Nathan Innes <nathaninnes001@gmail.com>
2022-11-03 01:56:06 +0000

    style: tag colour

--- src/components/Notam/Tag.tsx
+++ src/components/Notam/Tag.tsx
@@ -5,7 +5,7 @@ export interface TagProps {
 const handleCategory = (category: string) => {
     switch (category) {
     case 'UPDATE':
-        return 'border-green-500 bg-green-500/20 text-green-500';
+        return 'border-yellow-500 bg-yellow-500/20 text-yellow-500';
     case 'NEWS':
         return 'border-primary bg-primary/20 text-primary';
     default:
@@ -14,10 +14,10 @@ const handleCategory = (category: string) => {
 };
 
 const Tag = ({ category }: TagProps) => (
-    <div className={`max-w-min rounded-lg border py-1 px-4 ${handleCategory(category)}`}>
-        <h5>
+    <div className={`max-w-min rounded-lg border py-0.5 px-4 ${handleCategory(category)}`}>
+        <small>
             {category.charAt(0).toUpperCase() + category.slice(1).toLowerCase()}
-        </h5>
+        </small>
     </div>
 );
 
