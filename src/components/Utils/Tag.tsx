763338d9c9307bbdd77423982f8144c49271ebc4
Nathan Innes <nathaninnes001@gmail.com>
2023-01-27 01:54:19 +0000

    refactor: change tag location

--- src/components/Utils/Tag.tsx
+++ src/components/Utils/Tag.tsx
@@ -1,20 +1,18 @@
 export interface TagProps {
-    category: 'UPDATE' | 'NEWS';
+    category: 'Update' | 'News' | 'Aircraft' | 'Software';
 }
 
 const handleCategory = (category: string) => {
     switch (category) {
     case 'UPDATE':
         return 'border-yellow-500 bg-yellow-500/20 text-yellow-500';
-    case 'NEWS':
-        return 'border-primary bg-primary/20 text-primary';
     default:
-        return '';
+        return 'border-primary bg-primary/20 text-primary';
     }
 };
 
 const Tag = ({ category }: TagProps) => (
-    <div className={`max-w-min rounded-lg border py-0.5 px-4 ${handleCategory(category)}`}>
+    <div className={`max-w-min rounded-lg py-0.5 px-4 ${handleCategory(category)}`}>
         <small>
             {category.charAt(0).toUpperCase() + category.slice(1).toLowerCase()}
         </small>
