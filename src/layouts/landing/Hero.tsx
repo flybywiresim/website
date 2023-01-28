60dc94d1a9de55e72f6d2e6dbb1530ef4a5e35ef
Nathan Innes <nathaninnes001@gmail.com>
2023-01-28 13:03:18 +0000

    style: adjust hero view height

--- src/layouts/landing/Hero.tsx
+++ src/layouts/landing/Hero.tsx
@@ -18,9 +18,9 @@ const ButtonGroup = (props: {children: ReactNode}) => (
 
 const Hero = () => (
     <>
-        <Section className="bg-black/50 bg-gradient-to-bl from-primary/30 to-secondary">
+        <Section className="h-screen bg-black/50 bg-gradient-to-bl from-primary/30 to-secondary">
             <BackgroundImage />
-            <Container className="h-120 justify-center" display="flex flex-col">
+            <Container className="h-144 justify-center" display="flex flex-col">
                 <h4 className="font-normal leading-4 text-primary">FlyByWire Simulations</h4>
                 <h1>A32NX</h1>
                 <p className="max-w-xl">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alternation in some form, by injected humor...</p>
