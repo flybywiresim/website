78d0be34a7bcbbc12f2f281e38bc5cc8bf69a419
Nathan Innes <nathaninnes001@gmail.com>
2023-01-29 23:50:16 +0000

    fix: z-index

--- src/components/Button/ScrollToTopButton.tsx
+++ src/components/Button/ScrollToTopButton.tsx
@@ -7,7 +7,7 @@ const ScrollToTopButton = () => {
     return (
         <ArrowUpOutlined
             style={{ fontSize: '1.2em' }}
-            className={`fixed bottom-5 right-5 rounded-sm bg-primary p-3 ${visible ? 'block' : 'hidden'}`}
+            className={`fixed bottom-5 right-5 z-50 rounded-sm bg-primary p-3 ${visible ? 'block' : 'hidden'}`}
             onClick={handleClick}
         />
     );
