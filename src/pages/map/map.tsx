11259aa67d48af8bb10e1f3add18cce6075b1419
Nathan Innes <nathaninnes001@gmail.com>
2023-05-08 04:25:40 +0100

    style: adjust map pop up content layout

--- src/pages/map/map.tsx
+++ src/pages/map/map.tsx
@@ -2,7 +2,7 @@ import { FC, useEffect, useState } from 'react';
 import { Telex, TelexConnection } from '@flybywiresim/api-client';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { Icon } from 'leaflet';
-
+import { ArrowRightOutlined } from '@ant-design/icons';
 import Section from '../../components/Utils/Section';
 
 class AircraftIcon extends Icon {
@@ -29,8 +29,8 @@ const LeafletMap: FC = () => {
     const position = [51.505, -0.09];
 
     return (
-        <Section theme="dark" className="py-0">
-            <MapContainer center={position as any} zoom={13} scrollWheelZoom={false} style={{ width: '100%', height: '100vh' }}>
+        <Section theme="light" className="py-0">
+            <MapContainer center={position as any} zoom={13} scrollWheelZoom style={{ width: '100%', height: '100vh' }}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -38,36 +38,35 @@ const LeafletMap: FC = () => {
 
                 {flights.map((it) => (
                     <Marker key={it.id} icon={new AircraftIcon({ iconUrl: '' })} position={[it.location.y, it.location.x]}>
-                        <Popup className="" minWidth={360} maxWidth={360}>
-                            <div className="flex flex-col gap-y-4 bg-navy px-5 py-4 text-white rounded-xl" style={{ width: '360px' }}>
-                                <span className="font-display text-4xl text-primary font-bold">{it.flight}</span>
-
-                                <span className="font-body text-xl font-semibold text-gray-200">{it.aircraftType}</span>
-
-                                <span className="flex justify-center">
-                                    <span className="flex divide-x-2 divide-gray-600">
-                                        <span className="flex flex-col items-center px-8 text-2xl font-semibold">
-                                            <span className="font-mono">{it.heading.toFixed(0).padStart(3, '0')}</span>
-                                            <span className="font-display text-xl text-gray-400">HDG</span>
-                                        </span>
-
-                                        <span className="flex flex-col items-center px-8 text-2xl font-semibold">
-                                            <span className="font-mono">{numberFormat.format(it.trueAltitude)}</span>
-                                            <span className="font-display text-xl text-gray-400">ALT</span>
-                                        </span>
-
-                                        <span className="flex flex-col items-center px-8 text-2xl font-semibold">
-                                            <span className="font-mono">{numberFormat.format(342)}</span>
-                                            <span className="font-display text-xl text-gray-400">TAS</span>
-                                        </span>
+                        <Popup>
+                            <div className="bg-secondary p-4 text-white">
+                                <span className="flex items-center justify-between gap-x-4">
+                                    <h3 className="text-primary">
+                                        {it.flight}
+                                    </h3>
+                                    <span className="flex items-center gap-1">
+                                        <h3 className="font-mono">{it.origin || '----'}</h3>
+                                        <ArrowRightOutlined style={{ fontSize: '2rem' }} className="text-primary" />
+                                        <h3 className="font-mono">{it.destination || '----'}</h3>
                                     </span>
                                 </span>
+                                <p>{it.aircraftType}</p>
 
-                                <span className="flex">
-                                    <span className="font-mono text-3xl">{it.origin || '----'}</span>
-                                    <span className="font-mono text-3xl text-gray-400">/</span>
-                                    <span className="font-mono text-3xl">{it.destination || '----'}</span>
-                                </span>
+                                <div className="grid grid-cols-3 divide-x py-4 text-center">
+                                    <span>
+                                        <h3 className="p-0 font-mono">{it.heading.toFixed(0).padStart(3, '0')}</h3>
+                                        <p>HDG</p>
+                                    </span>
+                                    <span>
+                                        <h3 className="p-0 font-mono">{numberFormat.format(it.trueAltitude)}</h3>
+                                        <p>ALT</p>
+                                    </span>
+
+                                    <span>
+                                        <h3 className="p-0 font-mono">{numberFormat.format(342)}</h3>
+                                        <p>TAS</p>
+                                    </span>
+                                </div>
                             </div>
                         </Popup>
                     </Marker>
