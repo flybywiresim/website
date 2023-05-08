a16ab1ed4637887cfdf66f68a97bb42096dd2cc2
Nathan Innes <nathaninnes001@gmail.com>
2023-05-08 02:38:00 +0100

    style: adjust notam card height

--- src/components/Notam/Card.tsx
+++ src/components/Notam/Card.tsx
@@ -4,14 +4,12 @@ import Tag from '../Utils/Tag';
 import { PostListing } from '../../lib/notams/posts';
 
 const Card = (props: PostListing) => (
-    <div className="grid h-full cursor-pointer bg-secondary-accent-dark">
+    <div className="grid cursor-pointer bg-secondary-accent-dark">
         <span className="relative h-40 w-full">
             <Image src={props.metaImage} alt={props.metaAlt} layout="fill" objectFit="cover" />
         </span>
         <Container className="grid py-4">
-            <span className="flex items-center justify-between gap-4">
-                <Tag category={props.category} />
-            </span>
+            <Tag category={props.category} />
             <h3>{props.title}</h3>
             <p className="max-w-prose py-4">{props.description}</p>
             <small className="text-gray-400">
