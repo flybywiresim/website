96b9c8ea1676a9814030f16a843c8d310b9cbfa4
nathaninnes <nathaninnes001@gmail.com>
2023-05-12 12:48:00 +0100

    style(button): adjust width property to be more dynamic

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,13 +4,14 @@ type ButtonProps = {
     className?: string;
     onClick?: () => void
 }
+
 const Button = ({ label, theme, className, onClick }: ButtonProps) => (
     <button
         type="button"
         onClick={onClick}
         className={`
         ${theme === 'primary' ? 'bg-primary-accent text-secondary' : 'bg-secondary-accent-light text-light'} 
-        h-10 w-52 rounded-md px-4 py-1 font-body text-lg font-semibold
+        rounded-md px-12 max-w-max h-full py-1 font-body text-lg font-semibold
         ${className}
     `}
     >
