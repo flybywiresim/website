07923964a63db83c58ac5c95b79e110999d15c30
Louis Devota <hyperman738@gmail.com>
2024-07-05 18:37:44 -0700

    added hover state to cards (notams page)

--- src/components/Notam/Card.tsx
+++ src/components/Notam/Card.tsx
@@ -3,38 +3,40 @@ import Tag from '../Utils/Tag';
 import { PostListing } from '../../lib/notams/posts';
 
 const Card = (props: PostListing) => (
-    <div className={`
-    ${props.index === 0
-        ? 'grid grid-rows-2 xl:grid-rows-none xl:grid-cols-2 text-left min-h-[441px]'
-        : 'grid-rows-2 text-left min-h-[441px]'
-    } 
-        grid bg-secondary-accent-dark rounded-md overflow-hidden
-    `}
-    >
+    <div className="border-2 border-transparent hover:border-primary rounded-md overflow-hidden">
+        <div className={`
+        ${props.index === 0 
+            ? 'grid-rows-2 xl:grid-rows-none xl:grid-cols-2 text-left'
+            : 'grid-rows-2 text-left'
+        } 
+        grid bg-secondary-accent-dark min-h-[441px]
+        `}
+        >
         <span className="relative">
             <Image src={props.metaImage} alt={props.metaAlt} layout="fill" objectFit="cover" />
         </span>
-        <div className="">
-            <div className="px-8 py-6 flex justify-between">
-                <p className="text-gray-400">
-                    Posted:
-                    {' '}
-                    {new Date(props.date).toLocaleDateString('en-US', { dateStyle: 'long' })}
-                </p>
-                <span className="grid">
+            <div className="">
+                <div className="px-8 py-6 flex justify-between">
+                    <p className="text-gray-400">
+                        Posted:
+                        {' '}
+                        {new Date(props.date).toLocaleDateString('en-US', { dateStyle: 'long' })}
+                    </p>
+                    <span className="grid">
                     <div className="flex gap-x-3">
                         {props.index === 0 && <Tag category="Latest" />}
                         <Tag category={props.category} />
                     </div>
                 </span>
-            </div>
-            <div className="px-8 flex-col items-center">
-                <h3 className={props.index === 0 ? 'xl:text-7xl' : 'text-2xl'}>{props.title}</h3>
-                <small>
-                    By
-                    {' '}
-                    {props.authors?.join(', ')}
-                </small>
+                </div>
+                <div className="px-8 flex-col items-center">
+                    <h3 className={props.index === 0 ? 'xl:text-7xl' : 'text-2xl'}>{props.title}</h3>
+                    <small>
+                        By
+                        {' '}
+                        {props.authors?.join(', ')}
+                    </small>
+                </div>
             </div>
         </div>
     </div>
