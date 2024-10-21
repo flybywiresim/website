699f4e6860f065bacdf50a1cf1561b60692ea08a
Benjamin Dupont <4503241+Benjozork@users.noreply.github.com>
2024-10-21 00:23:44 -0400

    fix: map improvements

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -16,7 +16,13 @@ const AircraftIcon = (heading: number, aircraftId: string) => {
 
 const numberFormat = new Intl.NumberFormat();
 
-const LeafletMap: FC = () => {
+export interface LeafletMapProps {
+    isFullPageMap: boolean;
+
+    className?: string;
+}
+
+const LeafletMap: FC<LeafletMapProps> = ({ isFullPageMap, className }) => {
     const [flights, setFlights] = useState<TelexConnection[]>([]);
 
     useEffect(() => {
@@ -26,48 +32,50 @@ const LeafletMap: FC = () => {
     const position = [51, 5];
 
     return (
-        <MapContainer center={position as any} zoom={5} scrollWheelZoom style={{ width: '100%', height: '100vh' }}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
-            />
+        <div className={`${className} ${isFullPageMap ? 'full-page-map' : ''}`}>
+            <MapContainer center={position as any} zoom={5} scrollWheelZoom className="w-full h-full">
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+                />
 
-            {flights.map((it) => (
-                <Marker key={it.id} icon={AircraftIcon(it.heading, it.id)} position={[it.location.y, it.location.x]}>
-                    <Popup>
-                        <div className="bg-secondary p-4 text-white">
-                            <span className="flex items-center justify-between gap-x-4">
-                                <h3 className="text-primary">
-                                    {it.flight}
-                                </h3>
-                                <span className="flex items-center gap-1">
-                                    <h3 className="font-mono">{it.origin || '----'}</h3>
-                                    <ArrowRightOutlined style={{ fontSize: '2rem' }} className="text-primary" />
-                                    <h3 className="font-mono">{it.destination || '----'}</h3>
+                {flights.map((it) => (
+                    <Marker key={it.id} icon={AircraftIcon(it.heading, it.id)} position={[it.location.y, it.location.x]}>
+                        <Popup>
+                            <div className="bg-secondary p-4 text-white">
+                                <span className="flex items-center justify-between gap-x-4">
+                                    <h3 className="text-primary">
+                                        {it.flight}
+                                    </h3>
+                                    <span className="flex items-center gap-1">
+                                        <h3 className="font-mono">{it.origin || '----'}</h3>
+                                        <ArrowRightOutlined style={{ fontSize: '2rem' }} className="text-primary" />
+                                        <h3 className="font-mono">{it.destination || '----'}</h3>
+                                    </span>
                                 </span>
-                            </span>
-                            <p>{it.aircraftType}</p>
+                                <p>{it.aircraftType}</p>
 
-                            <div className="grid grid-cols-3 py-4 text-center">
-                                <span>
-                                    <h3 className="p-0 font-mono">{it.heading.toFixed(0).padStart(3, '0')}</h3>
-                                    <p>HDG</p>
-                                </span>
-                                <span>
-                                    <h3 className="p-0 font-mono">{numberFormat.format(it.trueAltitude)}</h3>
-                                    <p>ALT</p>
-                                </span>
+                                <div className="grid grid-cols-3 py-4 text-center">
+                                    <span>
+                                        <h3 className="p-0 font-mono">{it.heading.toFixed(0).padStart(3, '0')}</h3>
+                                        <p>HDG</p>
+                                    </span>
+                                    <span>
+                                        <h3 className="p-0 font-mono">{numberFormat.format(it.trueAltitude)}</h3>
+                                        <p>ALT</p>
+                                    </span>
 
-                                <span>
-                                    <h3 className="p-0 font-mono">{numberFormat.format(342)}</h3>
-                                    <p>TAS</p>
-                                </span>
+                                    <span>
+                                        <h3 className="p-0 font-mono">{numberFormat.format(342)}</h3>
+                                        <p>TAS</p>
+                                    </span>
+                                </div>
                             </div>
-                        </div>
-                    </Popup>
-                </Marker>
-            ))}
-        </MapContainer>
+                        </Popup>
+                    </Marker>
+                ))}
+            </MapContainer>
+        </div>
     );
 };
 
--- src/layouts/landing/Insights.tsx
+++ src/layouts/landing/Insights.tsx
@@ -28,35 +28,39 @@ const Insights = () => {
     }, []);
 
     return (
-        <Section theme="light" className="relative flex flex-col py-0 lg:flex-row-reverse">
-            <div className="flex flex-col items-center justify-center py-20">
+        <Section theme="light" className="relative flex flex-col py-0 xl:flex-row-reverse">
+            <div className="grow flex flex-col items-center justify-center py-20">
                 <div className="grid gap-y-4 px-4 text-center lg:text-left lg:px-16 2xl:px-28">
-                    <div className="flex flex-col items-center gap-4 lg:flex-row">
-                        <Image src="/svg/tail/tail.svg" width={40} height={40} />
-                        <h2>Community Insights</h2>
+                    <div className="flex flex-col max-lg:items-center">
+                        <div className="flex flex-col flex-wrap items-center gap-4 lg:flex-row">
+                            <Image src="/svg/tail/tail.svg" width={40} height={40} />
+
+                            <h2>Community Insights</h2>
+                        </div>
+
+                        <p className="max-w-prose max-lg:text-center">
+                            Discover the extensive community behind every FlyByWire Simulations aircraft - a vibrant and
+                            active online group that prioritises collaborative work and openness.
+                        </p>
                     </div>
 
-                    <div className="xl:max-w-3xl">
-                        <p>
-                            Discover the extensive community behind every FlyByWire Simulations aircraft - a vibrant and active online group that prioritises collaborative work and openness.
-                        </p>
-
+                    <div className="flex flex-col justify-center xl:max-w-3xl">
                         <ul className="grid grid-cols-2 gap-20 py-2 lg:grid-cols-4">
                             <Statistic value={liveFlights} label="Live Flights" />
                             <Statistic value={commitCount} label="Commits" />
                             <Statistic value={contributorCount} label="Contributors" />
                             <Statistic value="2M+" label="Downloads" />
                         </ul>
-                        <hr className="my-6" />
 
+                        <hr className="my-6" />
                         <Discord />
                     </div>
 
                 </div>
             </div>
 
-            <div className="relative w-[75%] px-8 pb-4 lg:mr-auto lg:p-0">
-                <LeafletMap />
+            <div className="min-w-[600px] max-xl:h-96 bg-red-500 grow-[3]">
+                <LeafletMap isFullPageMap={false} className="size-full" />
             </div>
         </Section>
     );
