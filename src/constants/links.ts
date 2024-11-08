053277b974418c42e404e5f55c316a9caf7a678f
NathanInnes <nathaninnes001@gmail.com>
2024-11-08 23:29:20 +0000

    fix: update cdn link to cf

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -7,6 +7,6 @@ export const links: {[name: string]: string} = {
     facebook: 'https://www.facebook.com/FlyByWireSimulations/',
     opencollective: 'https://opencollective.com/flybywire',
     docsfaq: 'https://docs.flybywiresim.com/faq',
-    installer: 'https://cdn.flybywiresim.com/installer/release/FlyByWire_Installer_Setup.exe',
+    installer: 'https://flybywirecdn.com/installer/release/FlyByWire_Installer_Setup.exe',
     efb: 'https://docs.flybywiresim.com/aircraft/common/flypados3',
 };
