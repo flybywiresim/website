93d0ba239522926742cf7ad1636015e69521f95d
LunakisDev <71980142+LunakisDev@users.noreply.github.com>
2024-11-08 19:57:09 +0200

    fix: Fixed the FBW Logo + Vercel Logo appearing in the map page (#529)
    * Fixed footer appearing in the map page
    
    * Fixed footer appearing in the map page with css
    
    * Revert map.tsx file to the original code
    
    * Revert fix
    
    * fixed logos appearing in the LeafletMap .
    
    * fix
    
    * Revert "Fixed footer appearing in the map page with css"
    
    * Update src/pages/map/index.tsx
    
    Co-authored-by: Nathan Innes <nathaninnes001@gmail.com>
    
    ---------
    
    Co-authored-by: Gaudv <gauthier@canque.fr>
    Co-authored-by: Gauthier Canque <10694444+Gaudv@users.noreply.github.com>
    Co-authored-by: Albert R <rojasalbert15@gmail.com>
    Co-authored-by: Nathan Innes <nathaninnes001@gmail.com>

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -6,8 +6,8 @@ import Section from '../../components/Utils/Section';
 const LeafletMap = dynamic(() => import('../../components/Map/Map'), { ssr: false });
 
 const Map: NextPage = () => (
-    <Section theme="dark" className="w-ful relative h-96 py-0">
-        <LeafletMap className="w-full h-[100vh]" isFullPageMap />
+    <Section theme="dark" className="w-screen h-screen py-0">
+        <LeafletMap className="w-full h-full" isFullPageMap />
     </Section>
 );
 
