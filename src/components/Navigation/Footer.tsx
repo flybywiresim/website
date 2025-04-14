256f5c8c770d9487a76d49d45a6876ada158886b
Philippe Dellaert <philippe@dellaert.org>
2025-04-14 10:03:27 -0700

    fix: bad link to the A32NX page in the footer (#538)

--- src/components/Navigation/Footer.tsx
+++ src/components/Navigation/Footer.tsx
@@ -40,7 +40,7 @@ const Footer = () => (
                         <div className="grid grid-cols-3 items-start gap-x-32 gap-y-5">
                             <NavWrapper label="Projects">
                                 <NavItem label="Installer" href={links.installer} />
-                                <NavItem label="A32NX" href="/projects/downloadLinks" />
+                                <NavItem label="A32NX" href="/projects/a32nx" />
                                 <NavItem label="A380X" href="/projects/a380x" />
                                 <NavItem label="EFB" href={links.efb} />
                             </NavWrapper>
