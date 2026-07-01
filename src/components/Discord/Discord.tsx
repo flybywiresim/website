c4147c824ce7cc66ecd06cc2ea9e408ae576f886
Cameron Carlyon <cameroncarlyon@gmail.com>
2026-07-01 16:18:02 +0100

    Added assets
    Added assets for A380X showcase, including video, fallbacks, and SVG. Also relocated discord.svg within the project structure for clarity. Updated reference in Discord.tsx

--- src/components/Discord/Discord.tsx
+++ src/components/Discord/Discord.tsx
@@ -6,7 +6,7 @@ import { links } from '../../constants/links';
 const Discord = () => (
     <div className="flex-col items-center lg:items-start flex gap-y-2.5">
         <span className="flex justify-center gap-x-5 lg:justify-start">
-            <Image src="/svg/discord.svg" layout="intrinsic" width={40} height={40} />
+            <Image src="/svg/social/discord.svg" layout="intrinsic" width={40} height={40} />
             <h3 className="text-discord">Discord</h3>
         </span>
         <p>Join us to chat with other members of the community, get started with contributing, or ask us a question!</p>
